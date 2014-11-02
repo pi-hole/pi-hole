5d0a76c21a7d164fb3996940580c287902298ffe
jacobsalmela <jacob.salmela@gmail.com>
2014-11-02 14:07:24 -0600

    typo

--- block hulu ads/setup-resolv.sh
+++ block hulu ads/setup-resolv.sh
@@ -3,6 +3,6 @@
 sudo mv /etc/resolv.conf /etc/resolv.conf.orig
 sudo mv /etc/resolv.conf.pihole /etc/resolv.conf
 # DNS config file
-sudo mv /etc/dnsmasq.conf /etc/rdnsmasq.conf.orig
+sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
 sudo mv /etc/dnsmasq.conf.pihole /etc/dnsmasq.conf
 sudo service dnsmasq start
