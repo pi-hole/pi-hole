5b0d217186148cdc7d0c5a9061170b6ecd26da92
jacobsalmela <jacob.salmela@gmail.com>
2014-11-02 14:05:27 -0600

    dnsmasq and resolv

--- block hulu ads/setup-resolv.sh
+++ block hulu ads/setup-resolv.sh
@@ -2,7 +2,6 @@
 # Resolver file
 sudo mv /etc/resolv.conf /etc/resolv.conf.orig
 sudo mv /etc/resolv.conf.pihole /etc/resolv.conf
-
 # DNS config file
 sudo mv /etc/dnsmasq.conf /etc/rdnsmasq.conf.orig
 sudo mv /etc/dnsmasq.conf.pihole /etc/dnsmasq.conf
