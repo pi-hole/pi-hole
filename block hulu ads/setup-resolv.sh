c68bb0184ecc27e6302ce7739c8c6b1c837c6d56
jacobsalmela <jacob.salmela@gmail.com>
2014-11-13 18:26:36 -0600

    removing resolve entries

--- block hulu ads/setup-resolv.sh
+++ block hulu ads/setup-resolv.sh
@@ -1,8 +1,6 @@
 # !/bin/bash
-# Resolver file
-sudo mv /etc/resolv.conf /etc/resolv.conf.orig
-sudo mv /etc/resolv.conf.pihole /etc/resolv.conf
 # DNS config file
+# Run as a local script since modifying it will disconnect the Internet connection
 sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
 sudo mv /etc/dnsmasq.conf.pihole /etc/dnsmasq.conf
 sudo service dnsmasq start
