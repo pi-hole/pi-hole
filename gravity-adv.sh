d83343a176b116910e768f02a9531e79af631078
jacobsalmela <jacob.salmela@gmail.com>
2015-04-15 09:37:21 -0500

    reduce list to 97k entries using pizzafritta's awk cmd

--- gravity-adv.sh
+++ gravity-adv.sh
@@ -73,7 +73,7 @@ function gravity_advanced()
 	# Remove lines with no dots (i.e. localhost, localdomain, etc)
 	echo -n "" > $origin/$supernova;for i in $origin/*.$justDomainsExtension;do grep '\.' $i >> $origin/$supernova;done
 	# Remove newlines, sort by TLD, remove duplicates
-	cat $origin/$supernova | sed $'s/\r$//' | awk -F. '{for (i=NF; i>1; --i) printf "%s.",$i;print $1}' | sort -t'.' -k1,2 | awk -F. '{for (i=NF; i>1; --i) printf "%s.",$i;print $1}' | uniq > $origin/$eventHorizion
+	cat $origin/$supernova | sed $'s/\r$//' | awk -F. '{for (i=NF; i>1; --i) printf "%s.",$i;print $1}' | sort -t'.' -k1,2 | awk -F. 'NR!=1&&substr($0,1,length(p))==p {next} {p=$0".";for (i=NF; i>1; --i) printf "%s.",$i;print $1}' | uniq > $origin/$eventHorizion
 	numberOf=$(cat $origin/$eventHorizion | wc -l | sed 's/^[ \t]*//')
 	echo "$numberOf unique domains trapped in the event horizon."
 	# Format domain list as address=/example.com/127.0.0.1
