11614263b8579943c39c595daa86496d4e8af1c5
jacobsalmela <jacob.salmela@gmail.com>
2015-07-25 13:05:13 -0500

    better version of LCD display stats

--- advanced/Scripts/chronometer.sh
+++ advanced/Scripts/chronometer.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
-# Infinite loop that can be used to display ad domains on a Pi touch screen
-# Continually watch the log file and display ad domains that are blocked being blocked
-# Set the pi user to log in automatically and add run this script from .bashrc
-clear
-echo ""
-echo "       +-+-+-+-+-+-+-+ "
-echo "           Pi-hole     "
-echo "       +-+-+-+-+-+-+-+ "
-echo ""
-echo "       A black hole for"
-echo "        Internet Ads   " 
-echo ""
-echo "     http://pi-hole.net"
-echo ""
-echo "     $(ifconfig eth0 | awk '/inet addr/ {print $2}' | cut -d':' -f2)"
-sleep 7
-# Look for only the entries that contain /etc/hosts, indicating the domain was found to be an advertisement
-tail -f /var/log/daemon.log | awk '/\/etc\/hosts/ {if ($7 != "address" && $7 != "name" && $7 != "/etc/hosts") print $7; else;}'
+# Displays Pi-hole stats on the Adafruit PiTFT 2.8" touch screen
+# Set the pi user to log in automatically and run this script from /etc/profile
+for (( ; ; ))
+do
+	clear
+	# Displays a colorful Pi-hole logo
+	toilet -f small -F gay Pi-hole
+	echo "        $(ifconfig eth0 | awk '/inet addr/ {print $2}' | cut -d':' -f2)"
+	echo ""
+	uptime | cut -d' ' -f11-
+	echo "-------------------------------"
+	# Uncomment to continually read the log file and display the current domain being blocked
+	#tail -f /var/log/daemon.log | awk '/\/etc\/hosts/ {if ($7 != "address" && $7 != "name" && $7 != "/etc/hosts") print $7; else;}'
+	
+	today=$(date "+%b %e")
+	todaysQueryCount=$(cat /var/log/daemon.log | grep "$today" | awk '/query/ {print $7}' | wc -l)
+	todaysQueryCountV4=$(cat /var/log/daemon.log | grep "$today" | awk '/query/ && /\[A\]/ {print $7}' | wc -l)
+	todaysQueryCountV6=$(cat /var/log/daemon.log | grep "$today" | awk '/query/ && /\[AAAA\]/ {print $7}' | wc -l)
+	todaysAdsEliminated=$(cat /var/log/daemon.log | grep "$today" | awk '/\/etc\/hosts/ {print $7}' | wc -l)
+	dividend=$(echo "$todaysAdsEliminated/$todaysQueryCount" | bc -l)
+	fp=$(echo "$dividend*100" | bc -l)
+	percentAds=$(echo ${fp:0:4})
+	
+	echo "Queries:       $todaysQueryCountV4 / $todaysQueryCountV6"
+	echo "Pi-holed:      $todaysAdsEliminated  ($percentAds%)"
+	sleep 5
+done
