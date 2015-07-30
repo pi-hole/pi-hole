024395b73be05415ddf23bf227baa586099c10e8
Dan <Fourdee@users.noreply.github.com>
2015-07-30 18:01:10 +0100

    Update chronometer.sh
    Missed a change todaysAdsEliminated to /\/etc\/pihole\/gravity.list/

--- advanced/Scripts/chronometer.sh
+++ advanced/Scripts/chronometer.sh
@@ -17,7 +17,7 @@ do
 	todaysQueryCount=$(cat /var/log/pihole.log | grep "$today" | awk '/query/ {print $7}' | wc -l)
 	todaysQueryCountV4=$(cat /var/log/pihole.log | grep "$today" | awk '/query/ && /\[A\]/ {print $7}' | wc -l)
 	todaysQueryCountV6=$(cat /var/log/pihole.log | grep "$today" | awk '/query/ && /\[AAAA\]/ {print $7}' | wc -l)
-	todaysAdsEliminated=$(cat /var/log/pihole.log | grep "$today" | awk '/\/etc\/hosts/ {print $7}' | wc -l)
+	todaysAdsEliminated=$(cat /var/log/pihole.log | grep "$today" | awk '/\/etc\/pihole\/gravity.list/ {print $7}' | wc -l)
 	dividend=$(echo "$todaysAdsEliminated/$todaysQueryCount" | bc -l)
 	fp=$(echo "$dividend*100" | bc -l)
 	percentAds=$(echo ${fp:0:4})
