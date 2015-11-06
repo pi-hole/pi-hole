23eece944308e75ca340b709d259cb15aabc28ad
Dan Schaper <dschaper@users.noreply.github.com>
2015-11-05 18:11:34 -0800

    Upstream Branch pseudo-rebase

--- gravity.sh
+++ gravity.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # http://pi-hole.net
-# Compiles a list of ad-serving domains by downloading them from multiple sources
+# Compiles a list of ad-serving domains by downloading them from multiple sources 
 
 # This script should only be run after you have a static IP address set on the Pi
 piholeIP=$(hostname -I)
@@ -8,7 +8,7 @@ piholeIP=$(hostname -I)
 # Ad-list sources--one per line in single quotes
 sources=('https://adaway.org/hosts.txt'
 'http://adblock.gjtech.net/?format=unix-hosts'
-#'http://adblock.mahakala.is/'
+'http://adblock.mahakala.is/'
 'http://hosts-file.net/.%5Cad_servers.txt'
 'http://www.malwaredomainlist.com/hostslist/hosts.txt'
 'http://pgl.yoyo.org/adservers/serverlist.php?'
@@ -19,6 +19,9 @@ sources=('https://adaway.org/hosts.txt'
 adList=/etc/pihole/gravity.list
 origin=/etc/pihole
 piholeDir=/etc/pihole
+if [[ -f $piholeDir/pihole.conf ]];then
+	. $piholeDir/pihole.conf
+fi
 justDomainsExtension=domains
 matter=pihole.0.matter.txt
 andLight=pihole.1.andLight.txt
@@ -27,15 +30,9 @@ eventHorizon=pihole.3.eventHorizon.txt
 accretionDisc=pihole.4.accretionDisc.txt
 eyeOfTheNeedle=pihole.5.wormhole.txt
 blacklist=$piholeDir/blacklist.txt
+latentBlacklist=$origin/latentBlacklist.txt
 whitelist=$piholeDir/whitelist.txt
 latentWhitelist=$origin/latentWhitelist.txt
-
-# After setting defaults, check if there's local overrides
-if [[ -r $piholeDir/pihole.conf ]];then
-    echo "** Local calibration requested..."
-	. $piholeDir/pihole.conf
-fi
-
 
 echo "** Neutrino emissions detected..."
 
@@ -57,12 +54,8 @@ function createSwapFile()
 	sudo dphys-swapfile setup
 	sudo dphys-swapfile swapon
 	}
-
-
-if [[ -n "$noSwap" ]]; then
-    # if $noSwap is set, don't do anything
-    :
-elif [[ -f /etc/dphys-swapfile ]];then
+	
+if [[ -f /etc/dphys-swapfile ]];then
 	swapSize=$(cat /etc/dphys-swapfile | grep -m1 CONF_SWAPSIZE | cut -d'=' -f2)
 	if [[ $swapSize != 500 ]];then
 		mv /etc/dphys-swapfile /etc/dphys-swapfile.orig
@@ -82,31 +75,55 @@ do
 	url=${sources[$i]}
 	# Get just the domain from the URL
 	domain=$(echo "$url" | cut -d'/' -f3)
-
+	
 	# Save the file as list.#.domain
 	saveLocation=$origin/list.$i.$domain.$justDomainsExtension
 
-		echo -n "Getting $domain list... "
-	# Use a case statement to download lists that need special cURL commands to complete properly
+	agent="Mozilla/10.0"
+	
+	echo -n "Getting $domain list... "
+
+	# Use a case statement to download lists that need special cURL commands 
+	# to complete properly and reset the user agent when required
 	case "$domain" in
-		"adblock.mahakala.is") data=$(curl -s -A 'Mozilla/5.0 (X11; Linux x86_64; rv:30.0) Gecko/20100101 Firefox/30.0' -e http://forum.xda-developers.com/ -z $saveLocation $url);;
+		"adblock.mahakala.is") 
+			agent='Mozilla/5.0 (X11; Linux x86_64; rv:30.0) Gecko/20100101 Firefox/30.0'
+			cmd="curl -e http://forum.xda-developers.com/"
+			;;
+		
+		"pgl.yoyo.org") 
+			cmd="curl -d mimetype=plaintext -d hostformat=hosts"
+			;;
 
-		"pgl.yoyo.org") data=$(curl -s -d mimetype=plaintext -d hostformat=hosts -z $saveLocation $url);;
-
-		*) data=$(curl -s -z $saveLocation -A "Mozilla/10.0" $url);;
+		# Default is a simple curl request
+		*) cmd="curl"
 	esac
 
-	if [[ -n "$data" ]];then
+	# tmp file, so we don't have to store the (long!) lists in RAM
+	tmpfile=`mktemp`
+	timeCheck=""
+	if [ -r $saveLocation ]; then 
+		timeCheck="-z $saveLocation"
+	fi
+	CMD="$cmd -s $timeCheck -A '$agent' $url > $tmpfile"
+	echo "running [$CMD]"
+	$cmd -s $timeCheck -A "$agent" $url > $tmpfile
+
+	
+	if [[ -s "$tmpfile" ]];then
 		# Remove comments and print only the domain name
 		# Most of the lists downloaded are already in hosts file format but the spacing/formating is not contigious
 		# This helps with that and makes it easier to read
 		# It also helps with debugging so each stage of the script can be researched more in depth
-		echo "$data" | awk 'NF {if ($1 !~ "#") { if (NF>1) {print $2} else {print $1}}}' | \
-			sed -e 's/^[. \t]*//' -e 's/\.\.\+/./g' -e 's/[. \t]*$//' | grep "\." > $saveLocation
+		awk '($1 !~ /^#/) { if (NF>1) {print $2} else {print $1}}' $tmpfile | \
+			sed -nr -e 's/\.{2,}/./g' -e '/\./p' > $saveLocation
 		echo "Done."
 	else
 		echo "Skipping list because it does not have any new entries."
 	fi
+
+	# cleanup
+	rm -f $tmpfile
 done
 
 # Find all files with the .domains extension and compile them into one file and remove CRs
@@ -114,39 +131,43 @@ echo "** Aggregating list of domains..."
 find $origin/ -type f -name "*.$justDomainsExtension" -exec cat {} \; | tr -d '\r' > $origin/$matter
 
 # Append blacklist entries if they exist
-if [[ -f $blacklist ]];then
+if [[ -r $blacklist ]];then
 	numberOf=$(cat $blacklist | sed '/^\s*$/d' | wc -l)
 	echo "** Blacklisting $numberOf domain(s)..."
 	cat $blacklist >> $origin/$matter
-else
-	:
 fi
 
-function gravity_advanced()
 ###########################
-	{
-	numberOf=$(cat $origin/$andLight | sed '/^\s*$/d' | wc -l)
-	echo "** $numberOf domains being pulled in by gravity..."
+function gravity_advanced() {
+
+	numberOf=$(wc -l $origin/$andLight)
+	echo "** $numberOf domains being pulled in by gravity..."	
+
 	# Remove carriage returns and preceding whitespace
-	cat $origin/$andLight | sed $'s/\r$//' | sed '/^\s*$/d' > $origin/$supernova
+	# not really needed anymore?
+	cp $origin/$andLight $origin/$supernova 
+
 	# Sort and remove duplicates
-	cat $origin/$supernova | sort | uniq > $origin/$eventHorizon
-	numberOf=$(cat $origin/$eventHorizon | sed '/^\s*$/d' | wc -l)
+	sort -u  $origin/$supernova > $origin/$eventHorizon
+	numberOf=$(wc -l $origin/$eventHorizon)
 	echo "** $numberOf unique domains trapped in the event horizon."
+
 	# Format domain list as "192.168.x.x domain.com"
 	echo "** Formatting domains into a HOSTS file..."
-	cat $origin/$eventHorizon | awk '{sub(/\r$/,""); print "'"$piholeIP"' " $0}' > $origin/$accretionDisc
+	awk '{print "'"$piholeIP"'" $1}' $origin/$eventHorizon > $origin/$accretionDisc
+
 	# Copy the file over as /etc/pihole/gravity.list so dnsmasq can use it
 	sudo cp $origin/$accretionDisc $adList
 	kill -HUP $(pidof dnsmasq)
-	}
-
+}
+	
 # Whitelist (if applicable) then remove duplicates and format for dnsmasq
-if [[ -f $whitelist ]];then
+if [[ -r $whitelist ]];then
 	# Remove whitelist entries
 	numberOf=$(cat $whitelist | sed '/^\s*$/d' | wc -l)
 	plural=; [[ "$numberOf" != "1" ]] && plural=s
 	echo "** Whitelisting $numberOf domain${plural}..."
+
 	# Append a "$" to the end, prepend a "^" to the beginning, and
 	# replace "." with "\." of each line to turn each entry into a
 	# regexp so it can be parsed out with grep -x
@@ -163,6 +184,7 @@ do
 	echo "$url" | awk -F '/' '{print "^"$3"$"}' | sed 's/\./\\./g' >> $latentWhitelist
 done
 
+# Remove whitelist entries from deduped list
 grep -vxf $latentWhitelist $origin/$matter > $origin/$andLight
 
 gravity_advanced
