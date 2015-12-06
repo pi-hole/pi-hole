22d0e82d92e4270a3c7620eccdab3b31e12004e8
Dan Schaper <dschaper@ganymeade.com>
2015-12-06 06:31:49 -0800

    Add GPL V2+ header

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -3,6 +3,13 @@
 # by Jacob Salmela
 # Network-wide ad blocking via your Raspberry Pi
 #
+# (c) 2015 by Jacob Salmela
+# This file is part of Pi-hole.
+#
+# Pi-hole is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
 # pi-hole.net/donate
 #
 # Install with this command (from your Pi):
