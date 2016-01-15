14ee4850afda52fc3677c810ff88f3033205cd22
Adam Warner <adzjwarner@gmail.com>
2016-01-15 14:50:13 +0000

    Placeholder for blacklist script

---
+++ advanced/Scripts/blacklist.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# (c) 2015 by Jacob Salmela
+# This file is part of Pi-hole.
+#
+# Pi-hole is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
