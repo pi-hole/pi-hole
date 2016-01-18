de464e17cc7b1f3bc4435f431364b65a830e55ad
Adam Warner <adzjwarner@gmail.com>
2016-01-18 22:24:02 +0000

    Update chronometer.sh
    Note to self, Find&Replace>Replace All is a bad idea.

--- advanced/Scripts/chronometer.sh
+++ advanced/Scripts/chronometer.sh
@@ -5,7 +5,7 @@
 # Pi-hole is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 2 of the License, or
-# (at your opercentBlockedTodayion) any later version.
+# (at your option) any later version.
 
 
 #Functions##############################################################################################################
