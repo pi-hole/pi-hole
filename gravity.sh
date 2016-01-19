1c87d9a6301c07335ba98a20c5e28546ccd11f98
jacobsalmela <jacobsalmela@users.noreply.github.com>
2016-01-18 22:25:46 -0600

    still don't git git

--- gravity.sh
+++ gravity.sh
@@ -4,9 +4,6 @@
 # Network-wide ad blocking via your Raspberry Pi
 # http://pi-hole.net
 # Compiles a list of ad-serving domains by downloading them from multiple sources
-<<<<<<< HEAD
-piholeIPfile=/tmp/piholeIP
-=======
 #
 # Pi-hole is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,20 +12,12 @@ piholeIPfile=/tmp/piholeIP
 
 piholeIPfile=/tmp/piholeIP
 piholeIPv6file=/etc/pihole/.useIPv6
->>>>>>> master
 if [[ -f $piholeIPfile ]];then
     # If the file exists, it means it was exported from the installation script and we should use that value instead of detecting it in this script
     piholeIP=$(cat $piholeIPfile)
     rm $piholeIPfile
 else
     # Otherwise, the IP address can be taken directly from the machine, which will happen when the script is run by the user and not the installation script
-<<<<<<< HEAD
-    piholeIP=$(ip -4 addr show | awk '{match($0,/[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+/); ip = substr($0,RSTART,RLENGTH); print ip}' | sed '/^\s*$/d' | grep -v "127.0.0.1")
-fi
-
-# Ad-list sources--one per line in single quotes
-# The mahakala source is commented out due to many users having issues with it blocking legitimate domains.  Uncomment at your own risk
-=======
     IPv4dev=$(ip route get 8.8.8.8 | awk '{for(i=1;i<=NF;i++)if($i~/dev/)print $(i+1)}')
     piholeIPCIDR=$(ip -o -f inet addr show dev $IPv4dev | awk '{print $4}' | awk 'END {print}')
     piholeIP=${piholeIPCIDR%/*}
@@ -42,7 +31,6 @@ fi
 # Ad-list sources--one per line in single quotes
 # The mahakala source is commented out due to many users having issues with it blocking legitimate domains.
 # Uncomment at your own risk
->>>>>>> master
 sources=('https://adaway.org/hosts.txt'
 'http://adblock.gjtech.net/?format=unix-hosts'
 #'http://adblock.mahakala.is/'
@@ -77,29 +65,6 @@ fi
 function gravity_collapse() {
 	echo "** Neutrino emissions detected..."
 
-<<<<<<< HEAD
-echo "** Neutrino emissions detected..."
-
-# Create the pihole resource directory if it doesn't exist.  Future files will be stored here
-if [[ -d $piholeDir ]];then
-	:
-else
-	echo "** Creating pihole directory..."
-	sudo mkdir $piholeDir
-fi
-
-# Add additional swap to prevent the "Error fork: unable to allocate memory" message:  https://github.com/jacobsalmela/pi-hole/issues/37
-function createSwapFile()
-#########################
-	{
-	echo "** Creating more swap space to accomodate large solar masses..."
-	sudo dphys-swapfile swapoff
-	sudo curl -s -o /etc/dphys-swapfile https://raw.githubusercontent.com/jacobsalmela/pi-hole/master/advanced/dphys-swapfile
-	sudo dphys-swapfile setup
-	sudo dphys-swapfile swapon
-	}
-
-=======
 	# Create the pihole resource directory if it doesn't exist.  Future files will be stored here
 	if [[ -d $piholeDir ]];then
         # Temporary hack to allow non-root access to pihole directory
@@ -112,7 +77,6 @@ function createSwapFile()
         mkdir $piholeDir
 	fi
 }
->>>>>>> master
 
 # patternCheck - check to see if curl downloaded any new files.
 function gravity_patternCheck() {
@@ -130,38 +94,6 @@ function gravity_patternCheck() {
 	fi
 }
 
-<<<<<<< HEAD
-# Loop through domain list.  Download each one and remove commented lines (lines beginning with '# 'or '/') and blank lines
-for ((i = 0; i < "${#sources[@]}"; i++))
-do
-	url=${sources[$i]}
-	# Get just the domain from the URL
-	domain=$(echo "$url" | cut -d'/' -f3)
-
-	# Save the file as list.#.domain
-	saveLocation=$origin/list.$i.$domain.$justDomainsExtension
-
-		echo -n "Getting $domain list... "
-	# Use a case statement to download lists that need special cURL commands to complete properly
-	case "$domain" in
-		"adblock.mahakala.is") data=$(curl -s -A 'Mozilla/5.0 (X11; Linux x86_64; rv:30.0) Gecko/20100101 Firefox/30.0' -e http://forum.xda-developers.com/ -z $saveLocation $url);;
-
-		"pgl.yoyo.org") data=$(curl -s -d mimetype=plaintext -d hostformat=hosts -z $saveLocation $url);;
-
-		*) data=$(curl -s -z $saveLocation -A "Mozilla/10.0" $url);;
-	esac
-
-	if [[ -n "$data" ]];then
-		# Remove comments and print only the domain name
-		# Most of the lists downloaded are already in hosts file format but the spacing/formating is not contigious
-		# This helps with that and makes it easier to read
-		# It also helps with debugging so each stage of the script can be researched more in depth
-		echo "$data" | awk 'NF {if ($1 !~ "#") { if (NF>1) {print $2} else {print $1}}}' | \
-			sed -e 's/^[. \t]*//' -e 's/\.\.\+/./g' -e 's/[. \t]*$//' | grep "\." > $saveLocation
-		echo "Done."
-	else
-		echo "Skipping list because it does not have any new entries."
-=======
 # transport - curl the specified url with any needed command extentions
 function gravity_transport() {
 	url=$1
@@ -174,7 +106,6 @@ function gravity_transport() {
 	if [[ -r $saveLocation ]]; then
 		# if domain has been saved, add file for date check to only download newer
 		heisenbergCompensator="-z $saveLocation"
->>>>>>> master
 	fi
 
 	# Silently curl url
@@ -186,15 +117,6 @@ function gravity_transport() {
 	rm -f $patternBuffer
 }
 
-<<<<<<< HEAD
-function gravity_advanced()
-###########################
-	{
-	numberOf=$(cat $origin/$andLight | sed '/^\s*$/d' | wc -l)
-	echo "** $numberOf domains being pulled in by gravity..."
-	# Remove carriage returns and preceding whitespace
-	cat $origin/$andLight | sed $'s/\r$//' | sed '/^\s*$/d' > $origin/$supernova
-=======
 # spinup - main gravity function
 function gravity_spinup() {
 
@@ -255,21 +177,20 @@ function gravity_Whitelist() {
 	# Prevent our sources from being pulled into the hole
 	plural=; [[ "${sources[@]}" != "1" ]] && plural=s
 	echo "** Whitelisting ${#sources[@]} ad list source${plural}..."
-	
+
 	urls=()
 	for url in ${sources[@]}
 	do
         tmp=$(echo "$url" | awk -F '/' '{print $3}')
         urls=("${urls[@]}" $tmp)
 	done
-	
+
 	whitelist.sh -f -nr -q ${urls[@]}
 
-		
+
 }
 
 function gravity_unique() {
->>>>>>> master
 	# Sort and remove duplicates
 	sort -u  $piholeDir/$supernova > $piholeDir/$eventHorizon
 	numberOf=$(wc -l < $piholeDir/$eventHorizon)
@@ -279,27 +200,6 @@ function gravity_unique() {
 function gravity_hostFormat() {
 	# Format domain list as "192.168.x.x domain.com"
 	echo "** Formatting domains into a HOSTS file..."
-<<<<<<< HEAD
-	cat $origin/$eventHorizon | awk '{sub(/\r$/,""); print "'"$piholeIP"' " $0}' > $origin/$accretionDisc
-	# Copy the file over as /etc/pihole/gravity.list so dnsmasq can use it
-	sudo cp $origin/$accretionDisc $adList
-	kill -HUP $(pidof dnsmasq)
-	}
-
-# Whitelist (if applicable) then remove duplicates and format for dnsmasq
-if [[ -f $whitelist ]];then
-	# Remove whitelist entries
-	numberOf=$(cat $whitelist | sed '/^\s*$/d' | wc -l)
-	plural=; [[ "$numberOf" != "1" ]] && plural=s
-	echo "** Whitelisting $numberOf domain${plural}..."
-	# Append a "$" to the end, prepend a "^" to the beginning, and
-	# replace "." with "\." of each line to turn each entry into a
-	# regexp so it can be parsed out with grep -x
-	awk -F '[# \t]' 'NF>0&&$1!="" {print "^"$1"$"}' $whitelist | sed 's/\./\\./g' > $latentWhitelist
-else
-	rm $latentWhitelist
-fi
-=======
   # If there is a value in the $piholeIPv6, then IPv6 will be used, so the awk command modified to create a line for both protocols
   if [[ -n $piholeIPv6 ]];then
     cat $piholeDir/$eventHorizon | awk -v ipv4addr="$piholeIP" -v ipv6addr="$piholeIPv6" '{sub(/\r$/,""); print ipv4addr" "$0"\n"ipv6addr" "$0}' > $piholeDir/$accretionDisc
@@ -327,7 +227,6 @@ function gravity_blackbody() {
 
 function gravity_advanced() {
 
->>>>>>> master
 
 	# Remove comments and print only the domain name
 	# Most of the lists downloaded are already in hosts file format but the spacing/formating is not contigious
