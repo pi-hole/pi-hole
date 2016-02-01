38e73a188a9570436a179b8be4333468f7a1bf67
Dan Schaper <dschaper@ganymeade.com>
2016-02-01 02:11:55 +0000

    Fix array selection

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -290,7 +290,7 @@ setDNS(){
 				piholeDNS1="8.8.8.8"
 				piholeDNS2="8.8.4.4"
 				;;
-			DynDNS)
+			OpenDNS)
 				echo "::: Using OpenDNS servers."
 				piholeDNS1="208.67.222.222"
 				piholeDNS2="208.67.220.220"
