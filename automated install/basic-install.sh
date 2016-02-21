a0673c7028c0e7124d1ebe9f2e68160ee70235a0
Adam Warner <PromoFaux@users.noreply.github.com>
2016-02-21 20:38:05 +0000

    Update basic-install.sh
    Increase required free bytes.

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -118,7 +118,7 @@ welcomeDialogs() {
 
 verifyFreeDiskSpace() {
 	# 25MB is the minimum space needed (20MB install + 5MB one day of logs.)
-	requiredFreeBytes=25600
+	requiredFreeBytes=51200
 	
 	existingFreeBytes=`df -lk / 2>&1 | awk '{print $4}' | head -2 | tail -1`    	
 	if ! [[ "$existingFreeBytes" =~ ^([0-9])+$ ]]; then       
