7014b5a150247ea832182c5b67efbc41aff456b3
Mcat12 <newtoncat12@yahoo.com>
2016-03-29 17:45:00 -0400

    Reverted password for web interface Work will continue in the secure-web-interface branch

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -658,19 +658,6 @@ setUser(){
 	fi
 }
 
-setPassword() {
-	# Password needed to authorize changes to lists from admin page
-	pass=$(whiptail --passwordbox "Please enter a password to secure your Pi-hole web interface." 10 50 3>&1 1>&2 2>&3)
-
-	if [ $? = 0 ]; then
-		# Entered password
-		echo $pass > /etc/pihole/password.txt
-	else
-		echo "::: Cancel selected, exiting...."
-		exit 1
-	fi
-}
-
 installPihole() {
 	# Install base files and web interface
 	checkForDependencies # done
@@ -720,9 +707,6 @@ use4andor6
 
 # Decide what upstream DNS Servers to use
 setDNS
-
-# Set the admin page password
-setPassword
 
 # Install and log everything to a file
 installPihole | tee $tmpLog
