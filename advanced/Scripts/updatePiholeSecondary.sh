f671eef3f089da4d061f1722d20321fbb758ad97
Promofaux <adzjwarner@gmail.com>
2016-04-08 23:43:31 +0100

    ground work on secondary update script

---
+++ advanced/Scripts/updatePiholeSecondary.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Pi-hole: A black hole for Internet advertisements
+# (c) 2015, 2016 by Jacob Salmela
+# Network-wide ad blocking via your Raspberry Pi
+# http://pi-hole.net
+# Checks if Pi-hole needs updating and then
+#
+# Pi-hole is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+
+function updateDependencies(){
+	newDependencies=()
+	echo "::: Installing any new dependencies..."
+	for i in "${newDependencies[@]}"; do
+		echo "checking for $i"
+	done
+	}
+}
