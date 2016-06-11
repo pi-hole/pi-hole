5c79184d3b58a956f360315bf5d1c48700a1925f
bcambl <blayne@blaynecampbell.com>
2016-06-11 17:17:12 -0600

    add ifconfig dependency for Fedora install

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -66,7 +66,7 @@ if [ -x "$(command -v rpm)" ];then
 	PKG_UPDATE="$PKG_MANAGER update -y"
 	PKG_INSTALL="$PKG_MANAGER install -y"
 	PKG_COUNT="$PKG_MANAGER check-update | grep -v ^Last | grep -c ^[a-zA-Z0-9]"
-	INSTALLER_DEPS=( iproute procps-ng newt dhcpcd )
+	INSTALLER_DEPS=( iproute net-tools procps-ng newt dhcpcd )
 	PIHOLE_DEPS=( epel-release bind-utils bc dnsmasq lighttpd lighttpd-fastcgi php-common php-cli php git curl unzip wget findutils cronie sudo )
 	LIGHTTPD_USER="lighttpd"
 	LIGHTTPD_GROUP="lighttpd"
