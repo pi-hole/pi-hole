612e22135f8c690d989b9ba5dd3f9c5db9388d2d
Promofaux <adzjwarner@gmail.com>
2016-09-28 21:14:53 +0100

    add back in needed script

---
+++ advanced/Scripts/piholeLogFlush.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Pi-hole: A black hole for Internet advertisements
+# (c) 2015, 2016 by Jacob Salmela
+# Network-wide ad blocking via your Raspberry Pi
+# http://pi-hole.net
+# Flushes /var/log/pihole.log
+#
+# Pi-hole is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+
+echo -n "::: Flushing /var/log/pihole.log ..."
+echo " " > /var/log/pihole.log
+echo "... done!"
