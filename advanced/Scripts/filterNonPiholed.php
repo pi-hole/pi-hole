2637d576102a9dd86304752bd73123b3a5741f58
Shadi Akiki <shadiakiki1986@gmail.com>
2016-09-30 21:10:18 +0000

    add file header and some documentation

--- advanced/Scripts/filterNonPiholed.php
+++ advanced/Scripts/filterNonPiholed.php
@@ -1,5 +1,28 @@
 <?php
 
+# Pi-hole: A black hole for Internet advertisements
+# (c) 2015, 2016 by Jacob Salmela
+# Network-wide ad blocking via your Raspberry Pi
+# http://pi-hole.net
+# Blacklists domains
+#
+# Pi-hole is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 2 of the License, or
+# (at your option) any later version.
+#
+#
+# This is a php script that uses the pi-hole admin dashboard
+# to get a list of queries, and then filters the json result
+# for the queries which were not pi-holed
+#
+# Usage: php filterNonPiholed.php
+#
+# Ref: https://github.com/pi-hole/AdminLTE#api
+#
+# Testing:
+# Can run pi-hole in docker using https://hub.docker.com/r/diginc/pi-hole/
+
 $url='http://localhost/admin/api.php?getAllQueries';
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_HEADER, false);
