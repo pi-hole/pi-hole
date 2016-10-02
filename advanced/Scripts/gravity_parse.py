e9ff14daf342e7bca931e109703971d90a8536df
Dan Schaper <dschaper@ganymeade.com>
2016-10-02 09:52:47 -0700

    Change `qt` (queryTable) to `gt` (gravityTable)

--- advanced/Scripts/gravity_parse.py
+++ advanced/Scripts/gravity_parse.py
@@ -1,35 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# Copyright (c) 2015, LCI Technology Group, LLC
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-#  Redistributions of source code must retain the above copyright notice, this
-#  list of conditions and the following disclaimer.
-#
-#  Redistributions in binary form must reproduce the above copyright notice,
-#  this list of conditions and the following disclaimer in the documentation
-#  and/or other materials provided with the distribution.
-#
-#  Neither the name of LCI Technology Group, LLC nor the names of its
-#  contributors may be used to endorse or promote products derived from this
-#  software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-
 # Pi-hole: A black hole for Internet advertisements
 # (c) 2015, 2016 by Jacob Salmela
 # Network-wide ad blocking via your Raspberry Pi
@@ -57,13 +28,13 @@ with conn:
     c.execute('DROP TABLE IF EXISTS gravity')
 
     # Ready new table for list of domains
-    qt = '''
+    gt = '''
     CREATE TABLE IF NOT EXISTS gravity (
         idx INTEGER PRIMARY KEY ASC,
         domain text
     )
     '''
-    c.execute(qt)
+    c.execute(gt)
 
     # enable WAL mode
     c.execute('PRAGMA journal_mode=WAL;')
