591208b7300c7e14b117cb657306fc59d9714d27
Adam Warner <awarner@croydon.parkeon.com>
2016-10-06 11:13:06 +0100

    remove gravity_parse.py and references

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -544,7 +544,6 @@ installScripts() {
 	${SUDO} cp /etc/.pihole/advanced/Scripts/version.sh /opt/pihole/version.sh
 
 	${SUDO} cp /etc/.pihole/advanced/Scripts/dnsmasq_parse.py /opt/pihole/dnsmasq_parse.py
- 	${SUDO} cp /etc/.pihole/advanced/Scripts/gravity_parse.py /opt/pihole/gravity_parse.py
  	${SUDO} cp /etc/.pihole/advanced/Scripts/summaryDB.py /opt/pihole/summaryDB.py
 
 	${SUDO} chmod 755 /opt/pihole/gravity.sh /opt/pihole/chronometer.sh /opt/pihole/whitelist.sh /opt/pihole/blacklist.sh /opt/pihole/piholeLogFlush.sh /opt/pihole/uninstall.sh /opt/pihole/setupLCD.sh /opt/pihole/version.sh
