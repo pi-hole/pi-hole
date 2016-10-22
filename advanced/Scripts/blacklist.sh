0f04e270a76b66c8cbf04f2697dc10182550f9df
Marcus Hildum <marcus.hildum@gmail.com>
2016-10-22 00:10:25 -0700

    consistency
    Just more cleanup

--- advanced/Scripts/blacklist.sh
+++ advanced/Scripts/blacklist.sh
@@ -47,119 +47,117 @@ domToRemoveList=()
 piholeIPfile=/etc/pihole/piholeIP
 piholeIPv6file=/etc/pihole/.useIPv6
 
-if [[ -f ${piholeIPfile} ]];then
-    # If the file exists, it means it was exported from the installation script and we should use that value instead of detecting it in this script
-    piholeIP=$(cat ${piholeIPfile})
-    #rm $piholeIPfile
+if [[ -f ${piholeIPfile} ]]; then
+	# If the file exists, it means it was exported from the installation script and we should use that value instead of detecting it in this script
+	piholeIP=$(cat ${piholeIPfile})
+	#rm $piholeIPfile
 else
-    # Otherwise, the IP address can be taken directly from the machine, which will happen when the script is run by the user and not the installation script
-    IPv4dev=$(ip route get 8.8.8.8 | awk '{for(i=1;i<=NF;i++)if($i~/dev/)print $(i+1)}')
-    piholeIPCIDR=$(ip -o -f inet addr show dev "$IPv4dev" | awk '{print $4}' | awk 'END {print}')
-    piholeIP=${piholeIPCIDR%/*}
+	# Otherwise, the IP address can be taken directly from the machine, which will happen when the script is run by the user and not the installation script
+	IPv4dev=$(ip route get 8.8.8.8 | awk '{for(i=1;i<=NF;i++)if($i~/dev/)print $(i+1)}')
+	piholeIPCIDR=$(ip -o -f inet addr show dev "$IPv4dev" | awk '{print $4}' | awk 'END {print}')
+	piholeIP=${piholeIPCIDR%/*}
 fi
 
 modifyHost=false
 
 # After setting defaults, check if there's local overrides
-if [[ -r ${piholeDir}/pihole.conf ]];then
-    echo "::: Local calibration requested..."
-        . ${piholeDir}/pihole.conf
+if [[ -r ${piholeDir}/pihole.conf ]]; then
+	echo "::: Local calibration requested..."
+	. ${piholeDir}/pihole.conf
 fi
 
 
-if [[ -f ${piholeIPv6file} ]];then
-    # If the file exists, then the user previously chose to use IPv6 in the automated installer
-    piholeIPv6=$(ip -6 route get 2001:4860:4860::8888 | awk -F " " '{ for(i=1;i<=NF;i++) if ($i == "src") print $(i+1) }')
+if [[ -f ${piholeIPv6file} ]]; then
+	# If the file exists, then the user previously chose to use IPv6 in the automated installer
+	piholeIPv6=$(ip -6 route get 2001:4860:4860::8888 | awk -F " " '{ for(i=1;i<=NF;i++) if ($i == "src") print $(i+1) }')
 fi
 
 HandleOther() {
-  #check validity of domain
-	validDomain=$(echo "$1" | perl -ne'print if /\b((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}\b/')
-	if [ -z "$validDomain" ]; then
-		echo "::: $1 is not a valid argument or domain name"
+	#check validity of domain
+	validDomain=$(echo "${1}" | perl -ne'print if /\b((?=[a-z0-9-]{1,63}\.)(xn--)?[a-z0-9]+(-[a-z0-9]+)*\.)+[a-z]{2,63}\b/')
+	if [ -z "${validDomain}" ]; then
+		echo "::: ${1} is not a valid argument or domain name"
 	else
-	  domList=("${domList[@]}" ${validDomain})
+		domList=("${domList[@]}" ${validDomain})
 	fi
 }
 
 PopBlacklistFile() {
 	#check blacklist file exists, and if not, create it
-	if [[ ! -f ${blacklist} ]];then
-  	  touch ${blacklist}
+	if [[ ! -f ${blacklist} ]]; then
+		touch ${blacklist}
 	fi
 	for dom in "${domList[@]}"; do
-	  if "$addmode"; then
-	  	AddDomain "$dom"
-	  else
-	    RemoveDomain "$dom"
-	  fi
+		if "${addmode}"; then
+			AddDomain "${dom}"
+		else
+			RemoveDomain "${dom}"
+		fi
 	done
 }
 
 AddDomain() {
-#| sed 's/\./\\./g'
+	#| sed 's/\./\\./g'
 	bool=false
 	grep -Ex -q "$1" ${blacklist} || bool=true
 	if ${bool}; then
-	  #domain not found in the blacklist file, add it!
-	  if ${verbose}; then
-	  echo -n "::: Adding $1 to blacklist file..."
-	  fi
-		echo "$1" >> ${blacklist}
+		#domain not found in the blacklist file, add it!
+		if ${verbose}; then
+			echo -n "::: Adding $1 to blacklist file..."
+		fi
+		echo "${1}" >> ${blacklist}
 		modifyHost=true
 		echo " done!"
 	else
 	if ${verbose}; then
-		echo "::: $1 already exists in $blacklist! No need to add"
+		echo "::: ${1} already exists in ${blacklist}! No need to add"
 		fi
 	fi
 }
 
 RemoveDomain() {
-
-  bool=false
-  grep -Ex -q "$1" ${blacklist} || bool=true
-  if ${bool}; then
-  	#Domain is not in the blacklist file, no need to Remove
-  	if ${verbose}; then
-  	echo "::: $1 is NOT blacklisted! No need to remove"
-  	fi
-  else
-    #Domain is in the blacklist file, add to a temporary array
-    if ${verbose}; then
-    echo "::: Un-blacklisting $dom..."
-    fi
-    domToRemoveList=("${domToRemoveList[@]}" $1)
-    modifyHost=true
-  fi
+	bool=false
+	grep -Ex -q "$1" ${blacklist} || bool=true
+	if ${bool}; then
+		#Domain is not in the blacklist file, no need to Remove
+		if ${verbose}; then
+			echo "::: $1 is NOT blacklisted! No need to remove"
+		fi
+	else
+		#Domain is in the blacklist file, add to a temporary array
+		if ${verbose}; then
+			echo "::: Un-blacklisting ${dom}..."
+		fi
+		domToRemoveList=("${domToRemoveList[@]}" $1)
+		modifyHost=true
+	fi
 }
 
 ModifyHostFile() {
-	 if ${addmode}; then
-	    #add domains to the hosts file
-	    if [[ -r ${blacklist} ]];then
-	      numberOf=$(cat ${blacklist} | sed '/^\s*$/d' | wc -l)
-        plural=; [[ "$numberOf" != "1" ]] && plural=s
-        echo ":::"
-        echo -n "::: Modifying HOSTS file to blacklist $numberOf domain${plural}..."
-	    	if [[ -n ${piholeIPv6} ]];then
+	if ${addmode}; then
+		#add domains to the hosts file
+		if [[ -r ${blacklist} ]]; then
+			numberOf=$(cat ${blacklist} | sed '/^\s*$/d' | wc -l)
+			plural=; [[ "${numberOf}" != "1" ]] && plural=s
+			echo ":::"
+			echo -n "::: Modifying HOSTS file to blacklist $numberOf domain${plural}..."
+			if [[ -n ${piholeIPv6} ]]; then
 				cat ${blacklist} | awk -v ipv4addr="$piholeIP" -v ipv6addr="$piholeIPv6" '{sub(/\r$/,""); print ipv4addr" "$0"\n"ipv6addr" "$0}' >> ${adList}
-	      	else
+			else
 				cat ${blacklist} | awk -v ipv4addr="$piholeIP" '{sub(/\r$/,""); print ipv4addr" "$0}' >>${adList}
-	      	fi
-	  	fi
-	  else
+			fi
+		fi
+	else
 		echo ":::"
-	  	for dom in "${domToRemoveList[@]}"
-		do
-	      #we need to remove the domains from the blacklist file and the host file
-			echo "::: $dom"
+		for dom in "${domToRemoveList[@]}"; do
+			#we need to remove the domains from the blacklist file and the host file
+			echo "::: ${dom}"
 			echo -n ":::    removing from HOSTS file..."
-	      	echo "$dom" | sed 's/\./\\./g' | xargs -I {} perl -i -ne'print unless /[^.]'{}'(?!.)/;' ${adList}
-	      	echo " done!"
-	      	echo -n ":::    removing from blackist.txt..."
-	      	echo "$dom" | sed 's/\./\\./g' | xargs -I {} perl -i -ne'print unless /'{}'(?!.)/;' ${blacklist}
-	      	echo " done!"
+			echo "${dom}" | sed 's/\./\\./g' | xargs -I {} perl -i -ne'print unless /[^.]'{}'(?!.)/;' ${adList}
+			echo " done!"
+			echo -n ":::    removing from blackist.txt..."
+			echo "${dom}" | sed 's/\./\\./g' | xargs -I {} perl -i -ne'print unless /'{}'(?!.)/;' ${blacklist}
+			echo " done!"
 		done
 	fi
 }
@@ -169,22 +167,22 @@ Reload() {
 	echo ":::"
 	echo -n "::: Refresh lists in dnsmasq..."
 
-    dnsmasqPid=$(pidof dnsmasq)
+	dnsmasqPid=$(pidof dnsmasq)
 
 	if [[ ${dnsmasqPid} ]]; then
-	    # service already running - reload config
-	    if [ -x "$(command -v systemctl)" ]; then
-            systemctl restart dnsmasq
-        else
-            service dnsmasq restart
-        fi
+		# service already running - reload config
+		if [ -x "$(command -v systemctl)" ]; then
+			systemctl restart dnsmasq
+		else
+			service dnsmasq restart
+		fi
 	else
-	    # service not running, start it up
-	    if [ -x "$(command -v systemctl)" ]; then
-            systemctl start dnsmasq
-        else
-            service dnsmasq start
-        fi
+		# service not running, start it up
+		if [ -x "$(command -v systemctl)" ]; then
+			systemctl start dnsmasq
+		else
+			service dnsmasq start
+		fi
 	fi
 	echo " done!"
 }
@@ -193,26 +191,24 @@ DisplayBlist() {
 	verbose=false
 	echo -e " Displaying Gravity Affected Domains \n"
 	count=1
-	while IFS= read -r AD
-	do
-		echo "${count}: $AD"
+	while IFS= read -r AD; do
+		echo "${count}: ${AD}"
 		count=$((count+1))
-	done < "$blacklist"
+	done < "${blacklist}"
 }
 
 ###################################################
 
-for var in "$@"
-do
-  case "$var" in
-    "-nr"| "--noreload"  ) reload=false;;
-    "-d" | "--delmode"   ) addmode=false;;
-    "-f" | "--force"     ) force=true;;
-    "-q" | "--quiet"     ) verbose=false;;
-    "-h" | "--help"	     ) helpFunc;;
-    "-l" | "--list"      ) DisplayBlist;;
-    *                    ) HandleOther "$var";;
-  esac
+for var in "$@"; do
+	case "$var" in
+		"-nr"| "--noreload"  ) reload=false;;
+		"-d" | "--delmode"   ) addmode=false;;
+		"-f" | "--force"     ) force=true;;
+		"-q" | "--quiet"     ) verbose=false;;
+		"-h" | "--help"      ) helpFunc;;
+		"-l" | "--list"      ) DisplayBlist;;
+		*                    ) HandleOther "$var";;
+	esac
 done
 
 PopBlacklistFile
@@ -220,7 +216,7 @@ PopBlacklistFile
 if ${modifyHost} || ${force}; then
 	ModifyHostFile
 else
-  if ${verbose}; then
+	if ${verbose}; then
 		echo "::: No changes need to be made"
 	fi
 	exit 1
