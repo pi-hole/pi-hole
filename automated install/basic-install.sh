5894a0e9c5b16e6e99aded5095c4fcc4fd343730
Tommy Huff <tuplink@gmail.com>
2016-10-24 17:53:07 -0400

    add python dependencie

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -104,7 +104,7 @@ elif [ -x "$(command -v rpm)" ]; then
 	PKG_INSTALL="${PKG_MANAGER} install -y"
 	PKG_COUNT="${PKG_MANAGER} check-update | egrep '(.i686|.x86|.noarch|.arm|.src)' | wc -l"
 	INSTALLER_DEPS=( iproute net-tools procps-ng newt git )
-	PIHOLE_DEPS=( epel-release bind-utils bc dnsmasq lighttpd lighttpd-fastcgi php-common php-cli php curl unzip wget findutils cronie sudo nmap-ncat )
+	PIHOLE_DEPS=( epel-release bind-utils bc dnsmasq lighttpd lighttpd-fastcgi php-common php-cli php curl unzip wget findutils cronie sudo nmap-ncat python )
 	if grep -q 'Fedora' /etc/redhat-release; then
 		remove_deps=(epel-release);
 		PIHOLE_DEPS=( ${PIHOLE_DEPS[@]/$remove_deps} );
