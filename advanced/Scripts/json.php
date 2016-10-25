56bbf3b05cd6a6d194f582dd743eb36fcc7754d3
Tommy Huff <tuplink@gmail.com>
2016-10-25 18:55:23 -0400

    Parse with PHP

---
+++ advanced/Scripts/json.php
@@ -0,0 +1,20 @@
+<?php
+if ((isset($argv[1])) && (isset($argv[2]))) {
+  $json="$argv[1]";
+  $key="$argv[2]";
+  $result=json_decode($json, true);
+  if (is_array($result)) {
+    if (array_key_exists($key, $result)) {
+      echo "$result[$key]";
+    } else {
+      //key dosnt exist
+      die("Error KEY");
+    }
+  } else {
+    //invalid json
+    die("Error JSON");
+  }
+} else {
+  die("Usage: php json.php \"JSON DATA HERE\" \"Key\"");
+}
+?>
