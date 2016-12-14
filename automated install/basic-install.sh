c10ec5548f0ce1c45052e80235e825120b9dde40
Promofaux <adzjwarner@gmail.com>
2016-12-14 19:29:44 +0000

    remove code that replaces tokens in 01-pihole.conf

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -612,36 +612,6 @@ version_check_dnsmasq() {
 		sed -i '/^server=@DNS2@/d' ${dnsmasq_pihole_01_location}
 	fi
 
-	#sed -i "s/@HOSTNAME@/$hostname/" ${dnsmasq_pihole_01_location}
-
-	if [[ -f /etc/hostname ]]; then
-		hostname=$(</etc/hostname)
-	elif [ -x "$(command -v hostname)" ]; then
-		hostname=$(hostname -f)
-	fi
-
-	#Replace IPv4 and IPv6 tokens in 01-pihole.conf for pi.hole resolution.
-	if [[ "${IPV4_ADDRESS}" != "" ]]; then
-	    tmp=${IPV4_ADDRESS%/*}
-	    sed -i "s/@IPv4@/$tmp/" ${dnsmasq_pihole_01_location}
-	else
-		sed -i '/^address=\/pi.hole\/@IPv4@/d' ${dnsmasq_pihole_01_location}
-		sed -i '/^address=\/@HOSTNAME@\/@IPv4@/d' ${dnsmasq_pihole_01_location}
-	fi
-
-	if [[ "${IPV6_ADDRESS}" != "" ]]; then
-	    sed -i "s/@IPv6@/$IPV6_ADDRESS/" ${dnsmasq_pihole_01_location}
-	else
-		sed -i '/^address=\/pi.hole\/@IPv6@/d' ${dnsmasq_pihole_01_location}
-		sed -i '/^address=\/@HOSTNAME@\/@IPv6@/d' ${dnsmasq_pihole_01_location}
-	fi
-
-	if [[ "${hostname}" != "" ]]; then
-	    sed -i "s/@HOSTNAME@/$hostname/" ${dnsmasq_pihole_01_location}
-	else
-		sed -i '/^address=\/@HOSTNAME@*/d' ${dnsmasq_pihole_01_location}
-	fi
-
 	sed -i 's/^#conf-dir=\/etc\/dnsmasq.d$/conf-dir=\/etc\/dnsmasq.d/' ${dnsmasq_conf}
 
 	if [[ "${QUERY_LOGGING}" == false ]] ; then
