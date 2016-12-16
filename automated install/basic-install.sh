d4c378ed5d1b0c21af598c12b2013547469b97f7
DL6ER <derigs@ph1.uni-koeln.de>
2016-12-16 13:20:25 +0100

    Remove PIHOLE_INTERFACE if already set (not PIHOLE_DOMAIN)

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -874,7 +874,7 @@ configureFirewall() {
 finalExports() {
 	# Update variables in setupVars.conf file
 	if [ -e "${setupVars}" ]; then
-		sed -i.update.bak '/PIHOLE_DOMAIN/d;/IPV4_ADDRESS/d;/IPV6_ADDRESS/d;/PIHOLE_DNS_1/d;/PIHOLE_DNS_2/d;/QUERY_LOGGING/d;' "${setupVars}"
+		sed -i.update.bak '/PIHOLE_INTERFACE/d;/IPV4_ADDRESS/d;/IPV6_ADDRESS/d;/PIHOLE_DNS_1/d;/PIHOLE_DNS_2/d;/QUERY_LOGGING/d;' "${setupVars}"
 	fi
     {
 	echo "PIHOLE_INTERFACE=${PIHOLE_INTERFACE}"
