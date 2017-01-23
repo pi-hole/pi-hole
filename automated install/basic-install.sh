31aa42c35e97c58717baac3e0c49e2478129962d
Dan Schaper <dschaper@ganymeade.com>
2017-01-23 14:28:56 -0800

    Transparency in `sudo` refire.

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -1060,7 +1060,7 @@ main() {
 
     if command -v sudo &> /dev/null; then
       echo "::: Utility sudo located."
-      exec curl -sSL https://install.pi-hole.net | sudo bash "$@"
+      exec curl -sSL https://raw.githubusercontent.com/pi-hole/pi-hole/master/automated%20install/basic-install.sh | sudo bash "$@"
       exit $?
     else
       echo "::: sudo is needed for the Web interface to run pihole commands.  Please run this script as root and it will be automatically installed."
