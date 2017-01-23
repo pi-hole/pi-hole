eb7b74d4f9321596270b0e9489980878b7e635c7
DL6ER <dl6er@dl6er.de>
2017-01-23 15:13:53 +0100

    Adding more debug echos

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -1211,18 +1211,26 @@ main() {
   fi
 
   echo "::: Restarting services..."
+  echo "Restarting dnsmasq"
   restart_service dnsmasq
+  echo "Enabling dnsmasq"
   enable_service dnsmasq
+  echo "done."
 
   if [ ! -f /etc/pihole/webupdate.running ] ; then
+    echo "Restarting lighttpd"
     restart_service lighttpd
     enable_service lighttpd
     echo "::: done."
   else
+    echo "Traping signals"
     trap "" SIGHUP SIGINT SIGTERM
+    echo "Traping signals"
     if [ -x "$(command -v systemctl)" ]; then
+      echo "Traping signals"
       setsid sh -c "nohup systemctl restart lighttpd </dev/null >/dev/null 2>&1 &"
     else
+      echo "Traping signals"
       setsid sh -c "nohup service lighttpd reload </dev/null >/dev/null 2>&1 &"
     fi
   fi
