c31b4383e6a0520fcedce65265a9b7241b8cc186
Promofaux <adzjwarner@gmail.com>
2017-01-28 21:37:21 +0000

    add back in code to stop `dnsmasq` and `lighttpd` during install.

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -1198,6 +1198,10 @@ main() {
     # Create directory for Pi-hole storage
     mkdir -p /etc/pihole/
 
+    stop_service dnsmasq
+    if [[ ${INSTALL_WEB} == true ]]; then
+      stop_service lighttpd
+    fi
     # Determine available interfaces
     get_available_interfaces
     # Find interfaces and let the user choose one
