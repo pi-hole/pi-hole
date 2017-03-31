02040cd25d4d4035fa0ae4b18ca63ad51ed34a9b
Adam Warner <adzjwarner@gmail.com>
2017-03-31 19:23:26 +0100

    We no longer need to do this, either

--- gravity.sh
+++ gravity.sh
@@ -403,8 +403,6 @@ if [[ "${forceGrav}" == true ]]; then
 	echo " done!"
 fi
 
-#Overwrite adlists.default from /etc/.pihole in case any changes have been made. Changes should be saved in /etc/adlists.list
-cp /etc/.pihole/adlists.default /etc/pihole/adlists.default
 gravity_collapse
 gravity_spinup
 if [[ "${skipDownload}" == false ]]; then
