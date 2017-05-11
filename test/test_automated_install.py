f5b76120138cf317132646254a6eea86b0bacf5c
Dan Schaper <dan.schaper@pi-hole.net>
2017-05-11 16:56:31 -0700

    Intentional fail to see the output from tests - squash this
    Signed-off-by: Dan Schaper <dan.schaper@pi-hole.net>

--- test/test_automated_install.py
+++ test/test_automated_install.py
@@ -411,10 +411,11 @@ def test_version_local_no_errors(Pihole):
     assert 'Current AdminLTE version is' in version_admin_local.stdout
     assert 'Current Pi-hole version is' in version_core_local.stdout
     assert 'Current FTL version is' in version_FTL_local.stdout
+    assert False
 
 def test_dummy(Pihole):
     Pihole.run('''
-    ls -lach /var/www/html
+    pihole
     ''')
     assert False
 
