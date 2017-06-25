c29c4ceb0f179f6e0ba0775edd265bdd7029987a
Dan Schaper <dan.schaper@pi-hole.net>
2017-06-25 14:21:41 -0700

    Integrate DL's update.sh fixes.
    `shellcheck -x` for following files.
    
    Do not test for included files SC1091
    
    Signed-off-by: Dan Schaper <dan.schaper@pi-hole.net>

--- test/test_shellcheck.py
+++ test/test_shellcheck.py
@@ -7,7 +7,7 @@ run_local = testinfra.get_backend(
 
 def test_scripts_pass_shellcheck():
     ''' Make sure shellcheck does not find anything wrong with our shell scripts '''
-    shellcheck = "find . -type f -name 'update.sh' | while read file; do shellcheck \"$file\" -e SC1090; done;"
+    shellcheck = "find . -type f -name 'update.sh' | while read file; do shellcheck -x \"$file\" -e SC1090,SC1091; done;"
     results = run_local(shellcheck)
     print results.stdout
     assert '' == results.stdout
