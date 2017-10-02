dcf6cd8f7e01494fc06bc2bd5de286bc51093a1d
Siddhu Arevindh <arevindh@gmail.com>
2017-10-02 13:00:21 +0530

    Fixed URL
    Fixed URL to current repo

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -1362,7 +1362,7 @@ main() {
 
     if command -v sudo &> /dev/null; then
       echo "::: Utility sudo located."
-      exec curl -sSL https://raw.githubusercontent.com/pi-hole/pi-hole/master/automated%20install/basic-install.sh | sudo bash "$@"
+      exec curl -sSL https://raw.githubusercontent.com/arevindh/pi-hole/master/automated%20install/basic-install.sh | sudo bash "$@"
       exit $?
     else
       echo "::: sudo is needed for the Web interface to run pihole commands.  Please run this script as root and it will be automatically installed."
