7e4630c07ae2b9d0a77f0635beb6c5c40f269651
Mausy5043 <Mausy5043@users.noreply.github.com>
2017-11-12 16:47:49 +0100

    change minus-sign to underscore in the filename

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -28,9 +28,9 @@ set -e
 # Local variables will be in lowercase and will exist only within functions
 # It's still a work in progress, so you may see some variance in this guideline until it is complete
 
-# We write to a temporary file before moving the log to the pihole folder
-tmpLog=/tmp/pihole-install.log
-instalLogLoc=/var/log/pihole-install.log
+# We write to a temporary file before moving the log to the /var/log folder
+tmpLog=/tmp/pihole_install.log
+instalLogLoc=/var/log/pihole_install.log
 # This is an important file as it contains information specific to the machine it's being installed on
 setupVars=/etc/pihole/setupVars.conf
 # Pi-hole uses lighttpd as a Web server, and this is the config file for it
