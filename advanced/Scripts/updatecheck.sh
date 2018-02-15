c09e7f870a3796dd4f644fe65304ca8b55cb3913
DL6ER <dl6er@dl6er.de>
2018-02-15 20:35:17 +0100

    Add exception in the update checker script to discard a possibly available update when the dnsmasq version is too low for extra logging and FTL's version is smaller than v4.0
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- advanced/Scripts/updatecheck.sh
+++ advanced/Scripts/updatecheck.sh
@@ -32,9 +32,25 @@ function get_local_branch() {
 }
 
 function get_local_version() {
-# Return active branch
-cd "${1}" 2> /dev/null || return 1
-git describe --long --dirty --tags || return 1
+  # Return active branch
+  cd "${1}" 2> /dev/null || return 1
+  git describe --long --dirty --tags || return 1
+}
+
+function extract_dnsmasq_version() {
+  # Return version of system-wide dnsmasq daemon
+  dnsmasq -v | awk '/Dnsmasq version/{print $3}'
+  # echo "2.72"
+  # echo "2.72-3"
+  # echo "2.73"
+}
+
+# Compare versions
+function version_cmp() {
+  # Use bash string comparison
+  [[ "$1" = "$2" ]] && echo 0
+  [[ "$1" > "$2" ]] && echo 1
+  [[ "$1" < "$2" ]] && echo 2
 }
 
 if [[ "$2" == "remote" ]]; then
@@ -43,9 +59,25 @@ if [[ "$2" == "remote" ]]; then
     sleep 30
   fi
 
+    # Locally available dnsmasq version is too old, don't suggest updating
   GITHUB_CORE_VERSION="$(json_extract tag_name "$(curl -q 'https://api.github.com/repos/pi-hole/pi-hole/releases/latest' 2> /dev/null)")"
   GITHUB_WEB_VERSION="$(json_extract tag_name "$(curl -q 'https://api.github.com/repos/pi-hole/AdminLTE/releases/latest' 2> /dev/null)")"
   GITHUB_FTL_VERSION="$(json_extract tag_name "$(curl -q 'https://api.github.com/repos/pi-hole/FTL/releases/latest' 2> /dev/null)")"
+  # GITHUB_FTL_VERSION="v3.8"
+  # GITHUB_FTL_VERSION="v4.0"
+  # GITHUB_FTL_VERSION="v4.1"
+
+  # We save the upstream version if either
+  #  - the dnsmasq version is at least 2.73, or
+  #  - the upstream version of FTL is at least v4.0
+  dnsmasqversion=$(version_cmp 2.73 $(extract_dnsmasq_version))
+  FTLupstreamversion=$(version_cmp v4.0 ${GITHUB_FTL_VERSION})
+  if [[ $dnsmasqversion == 1 && $FTLupstreamversion == 1 ]]; then
+    GITHUB_CORE_VERSION="$(get_local_version /etc/.pihole)"
+    GITHUB_WEB_VERSION="$(get_local_version /var/www/html/admin)"
+    GITHUB_FTL_VERSION="$(pihole-FTL version)"
+    echo "don't save tags"
+  fi
 
   echo -n "${GITHUB_CORE_VERSION} ${GITHUB_WEB_VERSION} ${GITHUB_FTL_VERSION}" > "/etc/pihole/GitHubVersions"
 
