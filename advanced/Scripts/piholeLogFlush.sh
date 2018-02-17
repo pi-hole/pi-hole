b8eaa9a52742437ba073927ee51bb7be2af9ced9
DL6ER <dl6er@dl6er.de>
2018-02-17 13:01:00 +0100

    Use double quotes to precent globbing and word splitting
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- advanced/Scripts/piholeLogFlush.sh
+++ advanced/Scripts/piholeLogFlush.sh
@@ -16,9 +16,9 @@ source ${colfile}
 # Constructed to return nothing when
 # a) the setting is not present in the config file, or
 # b) the setting is commented out (e.g. "#DBFILE=...")
-DBFILE=$(sed -n -e 's/^\s^.DBFILE\s*=\s*//p' /etc/pihole/pihole-FTL.conf)
+DBFILE="$(sed -n -e 's/^\s^.DBFILE\s*=\s*//p' /etc/pihole/pihole-FTL.conf)"
 # Test for empty string. Use standard path in this case.
-if [ -z $DBFILE ]; then
+if [ -z "$DBFILE" ]; then
   DBFILE="/etc/pihole/pihole-FTL.db"
 fi
 
