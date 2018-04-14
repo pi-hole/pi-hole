7923ae77a6adaed0b57095b0952f4c6d502c36ab
DL6ER <dl6er@dl6er.de>
2018-04-14 11:35:15 +0200

    Add shebang
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- advanced/pihole-FTL-prestart.sh
+++ advanced/pihole-FTL-prestart.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 /bin/touch /var/log/pihole-FTL.log /run/pihole-FTL.pid /run/pihole-FTL.port /var/log/pihole.log
 /bin/mkdir -p /var/run/pihole /var/log/pihole
 /bin/chown pihole:pihole /var/run/pihole /var/log/pihole
