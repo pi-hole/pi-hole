cd026cd865603bac7ca0e1b88b7e1bce5556c556
DL6ER <dl6er@dl6er.de>
2018-06-17 14:43:33 +0200

    Add comment where regexList is defined
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- advanced/Scripts/wildcard_regex_converter.sh
+++ advanced/Scripts/wildcard_regex_converter.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# Pi-hole: A black hole for Internet advertisements
+# (c) 2017 Pi-hole, LLC (https://pi-hole.net)
+# Network-wide ad blocking via your own hardware.
+#
+# Provides an automated migration subroutine to convert Pi-hole v3.x wildcard domains to Pi-hole v4.x regex filters
+#
+# This file is copyright under the latest version of the EUPL.
+# Please see LICENSE file for your rights under this license.
+
+# regexFile set in gravity.sh
 
 wildcardFile="/etc/dnsmasq.d/03-pihole-wildcard.conf"
 
