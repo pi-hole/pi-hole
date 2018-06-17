2809579dd736dfe5f5a5273c55777baf59e3eb8e
DL6ER <dl6er@dl6er.de>
2018-06-17 14:59:21 +0200

    Refine regex filter generation
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- advanced/Scripts/wildcard_regex_converter.sh
+++ advanced/Scripts/wildcard_regex_converter.sh
@@ -19,10 +19,10 @@ convert_wildcard_to_regex() {
   local addrlines domains uniquedomains
   # Obtain wildcard domains from old file
   addrlines="$(grep -oE "/.*/" ${wildcardFile})"
-  # Strip "/" from domain names
-  domains="$(sed 's/\///g;' <<< "${addrlines}")"
+  # Strip "/" from domain names and convert "." to regex-compatible "\."
+  domains="$(sed 's/\///g;s/\./\\./g' <<< "${addrlines}")"
   # Remove repeated domains (may have been inserted two times due to A and AAAA blocking)
   uniquedomains="$(uniq <<< "${domains}")"
   # Automatically generate regex filters and remove old wildcards file
-  awk '{print "(^)|(\\.)"$0"$"}' <<< "${uniquedomains}" >> "${regexFile:?}" && rm "${wildcardFile}"
+  awk '{print "((^)|(\\.))"$0"$"}' <<< "${uniquedomains}" >> "${regexFile:?}" && rm "${wildcardFile}"
 }
