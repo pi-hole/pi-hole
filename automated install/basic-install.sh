8b7667ba02a1a5ed9931f50ac2c1f9d50a9e5e2a
RamSet <RamSet@gmail.com>
2018-07-09 17:22:06 -0600

    Corrected function names, converted multiple line echos and converted 2 spaces to 4
    Signed-off-by: RamSet <RamSet@gmail.com>

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -149,7 +149,7 @@ show_ascii_berry() {
 
 # Compatibility
 
-53probe (){
+port_53_probe (){
   # Probe localhost via 127.0.0.1 for open port 53 and store process name if port 53 is occupied
   if (echo > /dev/tcp/127.0.0.1/53) >/dev/null 2>&1; then
     # What process is using 53?
@@ -157,10 +157,10 @@ show_ascii_berry() {
   fi
 }
 
-53check(){
+port_53_check(){
   # Print info regarding probing for open port 53
   echo -e "  ${INFO} Testing for port 53 availability...${COL_NC}"
-  53probe
+  port_53_probe
   # Check running process and see if it's blank
   if [ -z "$who53" ]; then
     echo -e "  ${TICK} ${COL_LIGHT_GREEN}Port 53 is available.${COL_NC}"
@@ -172,51 +172,51 @@ show_ascii_berry() {
     # If dnsmasq is present, set the dnsmasq-flag to true for future reference
     # (after packages and dependencies are installed).
     # dnsmasq will be disabled at end of install, prior to FTLDNS start.
-    echo -e "  ${EXCL} Port 53 is in use by ${COL_LIGHT_RED}$who53${COL_NC}. 
-    --forceFTLDNS flag was used. The installer ${COL_LIGHT_RED}will disable $who53${COL_NC} after dependencies and packages
-    have been downloaded, and replace dnsmasq with FTLDNS."
+    echo -e "  ${EXCL} Port 53 is in use by ${COL_LIGHT_RED}$who53${COL_NC}."
+    echo -e "  --forceFTLDNS flag was used. The installer ${COL_LIGHT_RED}will disable $who53${COL_NC} after dependencies and packages"
+    echo -e "  have been downloaded, and replace dnsmasq with FTLDNS."
     dnsmasq_flag=true
   elif [ "$who53" = "dnsmasq" ] && [[ $forceFTLDNS = "false" ]]; then
     # If dnsmasq is present, set the dnsmasq-flag to true for future reference
     # (after packages and dependencies are installed).
     # dnsmasq will be disabled at end of install, prior to FTLDNS start.
-    echo -e "  ${EXCL} Port 53 is in use by ${COL_LIGHT_RED}$who53${COL_NC}.
-    In order for the installer to proceed, ${COL_LIGHT_RED}$who53${COL_NC} needs to be disabled.
-    Please re-run the installer with the following command: 
-    ${COL_LIGHT_CYAN}curl -sSL https://install.pi-hole.net | bash -s -- --forceFTLDNS${COL_NC}"
+    echo -e "  ${EXCL} Port 53 is in use by ${COL_LIGHT_RED}$who53${COL_NC}."
+    echo -e "    In order for the installer to proceed, ${COL_LIGHT_RED}$who53${COL_NC} needs to be disabled."
+    echo -e "    Please re-run the installer with the following command:"
+    echo -e "    ${COL_LIGHT_CYAN}curl -sSL https://install.pi-hole.net | bash -s -- --forceFTLDNS${COL_NC}"
     exit 0
   elif [ "$who53" = "systemd-resolve" ] && [[ $forceFTLDNS = "true" ]]; then
     # If systemd-resolved is present, set the systemd-resolved-flag to true for future reference
     # (after packages and dependencies are installed).
     # systemd-resolved will be disabled at end of install, prior to FTLDNS start.
-    echo -e "  ${EXCL} Port 53 is in use by ${COL_LIGHT_RED}$who53${COL_NC}.
-      --forceFTLDNS flag was used. The installer ${COL_LIGHT_RED}will disable $who53${COL_NC} after dependencies and packages
-      have been downloaded, and replace the system DNS resolver with FTLDNS."
+    echo -e "  ${EXCL} Port 53 is in use by ${COL_LIGHT_RED}$who53${COL_NC}."
+    echo -e "    --forceFTLDNS flag was used. The installer ${COL_LIGHT_RED}will disable $who53${COL_NC} after dependencies and packages"
+    echo -e "    have been downloaded, and replace the system DNS resolver with FTLDNS."
       systemd_resolved_flag=true
   elif [ "$who53" = "systemd-resolve" ] && [[ $forceFTLDNS = "false" ]]; then
     # If systemd-resolved is present, set the systemd-resolved-flag to true for future reference
     # (after packages and dependencies are installed).
     # systemd-resolved will be disabled at end of install, prior to FTLDNS start.
-    echo -e "  ${EXCL} Port 53 is in use by ${COL_LIGHT_RED}$who53${COL_NC}.
-    In order for the installer to proceed, ${COL_LIGHT_RED}$who53${COL_NC} needs to be disabled.
-    Please re-run the installer with the following command:
-    ${COL_LIGHT_CYAN}curl -sSL https://install.pi-hole.net | bash -s -- --forceFTLDNS${COL_NC}"
+    echo -e "  ${EXCL} Port 53 is in use by ${COL_LIGHT_RED}$who53${COL_NC}."
+    echo -e "    In order for the installer to proceed, ${COL_LIGHT_RED}$who53${COL_NC} needs to be disabled."
+    echo -e "    Please re-run the installer with the following command:"
+    echo -e "    ${COL_LIGHT_CYAN}curl -sSL https://install.pi-hole.net | bash -s -- --forceFTLDNS${COL_NC}"
     exit 0
   else
     # Port 53 is used by something else, stop install
-    echo -e "  ${EXCL} ${COL_LIGHT_YELLOW}WARNING: Port 53 (mandatory for FTLDNS) is already in use by ${COL_LIGHT_RED}$who53${COL_NC}.
-      Since this will interfere with the functionality of FTLDNS, the installer cannot continue.
-      Please visit our discourse forum at ${COL_LIGHT_CYAN}https://discourse.pi-hole.net${COL_NC}
-      in order to get help related to this issue.
-      ${COL_LIGHT_RED}Installer will now exit.${COL_NC}"
+    echo -e "  ${EXCL} ${COL_LIGHT_YELLOW}WARNING: Port 53 (mandatory for FTLDNS) is already in use by ${COL_LIGHT_RED}$who53${COL_NC}."
+    echo -e "    Since this will interfere with the functionality of FTLDNS, the installer cannot continue."
+    echo -e "    Please visit our discourse forum at ${COL_LIGHT_CYAN}https://discourse.pi-hole.net${COL_NC}"
+    echo -e "    in order to get help related to this issue."
+    echo -e "    ${COL_LIGHT_RED}Installer will now exit.${COL_NC}"
     exit 0
   fi
 }
 
 # Creating silent port 53 check
-silent53check(){
+silentport_53_check(){
   # Probe localhost via 127.0.0.1 for open port 53 availability
-  53probe
+  port_53_probe
   # Check running process and see if it's dnsmasq
   if [ "$who53" = "dnsmasq" ]; then
     # If dnsmasq is present, set the dnsmasq-flag to true for future reference
@@ -1491,10 +1491,10 @@ disable_dnsmasq () {
     #setting dnsmasq_flag to false in order for the next check to validate or not
     dnsmasq_flag=false
     echo -e "  ${INFO} Checking if dnsmasq is still running"
-    silent53check
+    silentport_53_check
     if [[ $dnsmasq_flag = "true" ]]; then
-      echo -e "  ${EXCL} dnsmasq still active, this is most likely due to the fact that ${COL_LIGHT_RED}dnsmasq
-      was loaded via a non convetional method. This might cause future conflicts with FTLDNS${COL_NC}"
+      echo -e "  ${EXCL} dnsmasq still active, this is most likely due to the fact that ${COL_LIGHT_RED}dnsmasq"
+      echo -e "    was loaded via a non convetional method. This might cause future conflicts with FTLDNS${COL_NC}"
       ${SUDO} pkill dnsmasq
       echo -e "  ${TICK} dnsmasq process killed"
     fi
@@ -2400,7 +2400,7 @@ main() {
     # Show the Pi-hole logo so people know it's genuine since the logo and name are trademarked
     show_ascii_berry
     # Check for port 53 availability
-    53check
+    port_53_check
     make_temporary_log
   # Otherwise,
   else
