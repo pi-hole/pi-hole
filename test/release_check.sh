c674b5d5bcf1d25ea8debcfe76cf53b5fcbf74a5
Dan Schaper <dan.schaper@pi-hole.net>
2018-07-31 17:24:21 -0700

    Clone the repo and change to proper branch, then run new installer.
    Signed-off-by: Dan Schaper <dan.schaper@pi-hole.net>

--- test/release_check.sh
+++ test/release_check.sh
@@ -2,5 +2,8 @@
 
 mkdir /etc/pihole
 echo release/v4.0 | sudo tee /etc/pihole/ftlbranch
-curl -sSL https://raw.githubusercontent.com/pi-hole/pi-hole/release/v4.0/automated%20install/basic-install.sh | bash
+git clone https://github.com/pi-hole/pi-hole.git /etc/.pihole
+cd /etc/.pihole
+git checkout release/v4.0
+bash /etc/.pihole/automated\ install/basic-install.sh
 pihole checkout web release/v4.0
