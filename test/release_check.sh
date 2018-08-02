abc66e4f705a1ad1ab061f9fa8208592e4f1617e
Mark Drobnak <mark.drobnak@gmail.com>
2018-08-01 23:02:38 -0400

    Send "y" to the web checkout prompt
    Currently, it will fail because it does not detect "y" from stdin.
    
    Signed-off-by: Mark Drobnak <mark.drobnak@gmail.com>

--- test/release_check.sh
+++ test/release_check.sh
@@ -6,4 +6,4 @@ git clone https://github.com/pi-hole/pi-hole.git /etc/.pihole
 cd /etc/.pihole
 git checkout release/v4.0
 bash /etc/.pihole/automated\ install/basic-install.sh
-pihole checkout web release/v4.0
+pihole checkout web release/v4.0 <<< y
