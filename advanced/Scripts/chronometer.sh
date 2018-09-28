24e1dc92ba72a991d5c38fce6d28012dc6234fe9
WaLLy3K <WaLLy3K@users.noreply.github.com>
2018-09-28 10:34:49 +1000

    Resolve issue #2428
    Signed-off-by: WaLLy3K <wally3k@pi-hole.net>

--- advanced/Scripts/chronometer.sh
+++ advanced/Scripts/chronometer.sh
@@ -8,6 +8,7 @@
 #
 # This file is copyright under the latest version of the EUPL.
 # Please see LICENSE file for your rights under this license.
+LC_ALL=C
 LC_NUMERIC=C
 
 # Retrieve stats from FTL engine
