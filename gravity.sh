5c621fa2535416e5586adb061ea081f8f611b761
Dan Schaper <dan.schaper@pi-hole.net>
2018-11-12 17:21:34 -0800

    Lints
    Signed-off-by: Dan Schaper <dan.schaper@pi-hole.net>

--- gravity.sh
+++ gravity.sh
@@ -257,11 +257,11 @@ gravity_DownloadBlocklistFromUrl() {
    esac
 
   if [[ "${blocked}" == true ]]; then
-    printf -v ip_addr "%s" ${PIHOLE_DNS_1%#*}
+    printf -v ip_addr "%s" "${PIHOLE_DNS_1%#*}"
     if [[ ${PIHOLE_DNS_1} != *"#"* ]]; then
         port=53
     else
-        printf -v port "%s" ${PIHOLE_DNS_1#*#}
+        printf -v port "%s" "${PIHOLE_DNS_1#*#}"
     fi
     ip=$(dig "@${ip_addr}" -p "${port}" +short "${domain}")
     if [[ $(echo "${url}" | awk -F '://' '{print $1}') = "https" ]]; then
