3f05efd60f4e4ff5630621d3a1d50fd81e1e1807
Mcat12 <newtoncat12@yahoo.com>
2019-05-20 21:02:31 -0700

    Add extra newline
    Signed-off-by: Mcat12 <newtoncat12@yahoo.com>

--- advanced/Scripts/piholeDebug.sh
+++ advanced/Scripts/piholeDebug.sh
@@ -1076,6 +1076,7 @@ analyze_gravity_list() {
     local gravity_size
     gravity_size=$(sqlite3 "${PIHOLE_GRAVITY_DB_FILE}" "SELECT COUNT(*) FROM vw_gravity")
     log_write "   Size: ${COL_CYAN}${gravity_size}${COL_NC} entries"
+    log_write ""
 
     OLD_IFS="$IFS"
     IFS=$'\r\n'
