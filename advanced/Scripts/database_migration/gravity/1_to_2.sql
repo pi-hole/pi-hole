0c8f5f12215eaceb370e5110d8383fe34a428b17
DL6ER <dl6er@dl6er.de>
2019-07-05 14:06:05 +0200

    Remove comment field from audit table
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- advanced/Scripts/database_migration/gravity/1_to_2.sql
+++ advanced/Scripts/database_migration/gravity/1_to_2.sql
@@ -2,8 +2,7 @@ CREATE TABLE audit
 (
 	id INTEGER PRIMARY KEY AUTOINCREMENT,
 	domain TEXT UNIQUE NOT NULL,
-	date_added INTEGER NOT NULL DEFAULT (cast(strftime('%s', 'now') as int)),
-	comment TEXT
+	date_added INTEGER NOT NULL DEFAULT (cast(strftime('%s', 'now') as int))
 );
 
 UPDATE info SET value = 2 WHERE property = 'version';
