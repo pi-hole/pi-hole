852f6b95f5d10847569c1c59453aeaac7a03cef4
Thomas L. Kjeldsen <tlk@closureconsulting.com>
2019-08-23 19:49:13 +0200

    Prevent "pihole disable $timeout" from messing up future state changes
    Signed-off-by: Thomas L. Kjeldsen <tlk@closureconsulting.com>

---
+++ advanced/Scripts/pihole-reenable.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Pi-hole: A black hole for Internet advertisements
+# (c) 2019 Pi-hole, LLC (https://pi-hole.net)
+# Network-wide ad blocking via your own hardware.
+#
+# Wrapper script to re-enable Pi-hole after a period of it being disabled.
+#
+# This script will be aborted by the `pihole enable` command using `killall`.
+# As a consequence, the filename of this script is limited to max 15 characters
+# and the script does not use `env`.
+#
+# This file is copyright under the latest version of the EUPL.
+# Please see LICENSE file for your rights under this license.
+
+sleep "${1}"
+pihole enable
