ff08add7c0a42536d8901af8e325d7ffe861d887
DL6ER <dl6er@dl6er.de>
2019-09-07 13:01:36 +0200

    Update vw_whitelist and vw_blacklist to return group_id alongside domain so we can filter if the current client wants to get this domain blocked or not.
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- advanced/Scripts/database_migration/gravity/3_to_4.sql
+++ advanced/Scripts/database_migration/gravity/3_to_4.sql
@@ -13,12 +13,34 @@ CREATE TABLE gravity
 );
 
 DROP VIEW vw_gravity;
-CREATE VIEW vw_gravity AS SELECT domain, gravity.adlist_id
+CREATE VIEW vw_gravity AS SELECT domain, adlist_by_group.group_id AS group_id
     FROM gravity
     LEFT JOIN adlist_by_group ON adlist_by_group.adlist_id = gravity.adlist_id
     LEFT JOIN adlist ON adlist.id = gravity.adlist_id
     LEFT JOIN "group" ON "group".id = adlist_by_group.group_id
     WHERE adlist.enabled = 1 AND (adlist_by_group.group_id IS NULL OR "group".enabled = 1);
+
+DROP VIEW vw_whitelist;
+CREATE VIEW vw_whitelist AS SELECT domain, whitelist_by_group.group_id AS group_id
+    FROM whitelist
+    LEFT JOIN whitelist_by_group ON whitelist_by_group.whitelist_id = whitelist.id
+    LEFT JOIN "group" ON "group".id = whitelist_by_group.group_id
+    WHERE whitelist.enabled = 1 AND (whitelist_by_group.group_id IS NULL OR "group".enabled = 1)
+    ORDER BY whitelist.id;
+
+DROP VIEW vw_blacklist;
+CREATE VIEW vw_blacklist AS SELECT domain, blacklist_by_group.group_id AS group_id
+    FROM blacklist
+    LEFT JOIN blacklist_by_group ON blacklist_by_group.blacklist_id = blacklist.id
+    LEFT JOIN "group" ON "group".id = blacklist_by_group.group_id
+    WHERE blacklist.enabled = 1 AND (blacklist_by_group.group_id IS NULL OR "group".enabled = 1)
+    ORDER BY blacklist.id;
+
+CREATE TABLE client
+(
+	ip TEXT NOL NULL PRIMARY KEY,
+	"groups" TEXT NOT NULL
+);
 
 UPDATE info SET value = 4 WHERE property = 'version';
 
