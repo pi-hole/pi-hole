3d77b83a32b89633b0a0b228ca7d1fdb87f020fc
Jeroen van Leusen <jvleusen@gmail.com>
2019-09-24 10:45:56 +0200

    Adjust fix_files.sh license text
    Signed-off-by: Jeroen van Leusen <jvleusen@gmail.com>

--- advanced/fix_files.sh
+++ advanced/fix_files.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # Pi-hole: A black hole for Internet advertisements
-# (c) 2017 Pi-hole, LLC (https://pi-hole.net)
+# (c) 2019 Pi-hole, LLC (https://pi-hole.net)
 # Network-wide ad blocking via your own hardware.
 #
-# Completely uninstalls Pi-hole
+# Adjust file permissions for pihole-FTL
 #
 # This file is copyright under the latest version of the EUPL.
 # Please see LICENSE file for your rights under this license.
