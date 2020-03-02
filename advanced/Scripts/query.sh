4f390ce801c739ea87cfcae131855ecbd9161818
Dan Schaper <dan.schaper@pi-hole.net>
2020-03-02 05:39:21 -0800

    Use bash regex instead of awk.
    Signed-off-by: Dan Schaper <dan.schaper@pi-hole.net>

--- advanced/Scripts/query.sh
+++ advanced/Scripts/query.sh
@@ -33,15 +33,13 @@ scanList(){
     export LC_CTYPE=C
 
     # /dev/null forces filename to be printed when only one list has been generated
-    # shellcheck disable=SC2086
     case "${type}" in
         "exact" ) grep -i -E -l "(^|(?<!#)\\s)${esc_domain}($|\\s|#)" ${lists} /dev/null 2>/dev/null;;
         # Create array of regexps
         # Iterate through each regexp and check whether it matches the domainQuery
         # If it does, print the matching regexp and continue looping
         # Input 1 - regexps | Input 2 - domainQuery
-        "regex" ) awk 'NR==FNR{regexps[$0];next}{for (r in regexps)if($0 ~ r)print r}' \
-                  <(echo "${lists}") <(echo "${domain}") 2>/dev/null;;
+        "regex" ) if [[ "${domain}" =~ ${lists} ]]; then printf "%b\n" "${lists}"; fi;;
         *       ) grep -i "${esc_domain}" ${lists} /dev/null 2>/dev/null;;
     esac
 }
