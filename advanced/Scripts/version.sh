851947bbf2fa89defc972b8991e612cd95f24566
Adam Warner <me@adamwarner.co.uk>
2020-04-13 20:58:46 +0100

    Add branch name to version output
    Signed-off-by: Adam Warner <me@adamwarner.co.uk>

--- advanced/Scripts/version.sh
+++ advanced/Scripts/version.sh
@@ -97,28 +97,52 @@ getRemoteVersion(){
     return 0
 }
 
+getLocalBranch(){
+     # Local FTL btranch is stored in /etc/pihole/ftlbranch
+    if [[ "$1" == "FTL" ]]; then
+        cat /etc/pihole/ftlbranch
+        return 0
+    fi
+
+    # Get the checked out branch of the local directory
+    local directory="${1}"
+    local branch
+
+    cd "${directory}" 2> /dev/null || { echo "${DEFAULT}"; return 1; }
+    branch=$(git rev-parse --abbrev-ref HEAD || echo "$DEFAULT")
+    if [[ ! "${branch}" =~ ^v ]]; then
+        echo "${branch}"
+    elif [[ "${branch}" == "${DEFAULT}" ]]; then
+        echo "ERROR"
+        return 1
+    else
+        echo "No Checked out branch"
+    fi
+    return 0
+}
+
 versionOutput() {
     [[ "$1" == "pi-hole" ]] && GITDIR=$COREGITDIR
     [[ "$1" == "AdminLTE" ]] && GITDIR=$WEBGITDIR
     [[ "$1" == "FTL" ]] && GITDIR="FTL"
 
-    [[ "$2" == "-c" ]] || [[ "$2" == "--current" ]] || [[ -z "$2" ]] && current=$(getLocalVersion $GITDIR)
+    [[ "$2" == "-c" ]] || [[ "$2" == "--current" ]] || [[ -z "$2" ]] && current=$(getLocalVersion $GITDIR) && branch=$(getLocalBranch $GITDIR)
     [[ "$2" == "-l" ]] || [[ "$2" == "--latest" ]] || [[ -z "$2" ]] && latest=$(getRemoteVersion "$1")
     if [[ "$2" == "-h" ]] || [[ "$2" == "--hash" ]]; then
-        [[ "$3" == "-c" ]] || [[ "$3" == "--current" ]] || [[ -z "$3" ]] && curHash=$(getLocalHash "$GITDIR")
+        [[ "$3" == "-c" ]] || [[ "$3" == "--current" ]] || [[ -z "$3" ]] && curHash=$(getLocalHash "$GITDIR") && branch=$(getLocalBranch $GITDIR)
         [[ "$3" == "-l" ]] || [[ "$3" == "--latest" ]] || [[ -z "$3" ]] && latHash=$(getRemoteHash "$1" "$(cd "$GITDIR" 2> /dev/null && git rev-parse --abbrev-ref HEAD)")
     fi
 
     if [[ -n "$current" ]] && [[ -n "$latest" ]]; then
-        output="${1^} version is $current (Latest: $latest)"
+        output="${1^} version is $current (Latest: $latest) Branch: $branch"
     elif [[ -n "$current" ]] && [[ -z "$latest" ]]; then
-        output="Current ${1^} version is $current"
+        output="Current ${1^} version is $current. Branch: $branch"
     elif [[ -z "$current" ]] && [[ -n "$latest" ]]; then
         output="Latest ${1^} version is $latest"
     elif [[ "$curHash" == "N/A" ]] || [[ "$latHash" == "N/A" ]]; then
         output="${1^} hash is not applicable"
     elif [[ -n "$curHash" ]] && [[ -n "$latHash" ]]; then
-        output="${1^} hash is $curHash (Latest: $latHash)"
+        output="${1^} hash is $curHash (Latest: $latHash) Branch: $branch"
     elif [[ -n "$curHash" ]] && [[ -z "$latHash" ]]; then
         output="Current ${1^} hash is $curHash"
     elif [[ -z "$curHash" ]] && [[ -n "$latHash" ]]; then
