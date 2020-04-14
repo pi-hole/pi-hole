5712683f845571297330de1ac401b3826450393a
Andrea Scarpino <andrea@scarpino.dev>
2020-04-14 09:50:22 +0200

    REMOVE ME - point to my fork
    Signed-off-by: Andrea Scarpino <andrea@scarpino.dev>

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -58,7 +58,7 @@ webroot="/var/www/html"
 # We store several other directories and
 webInterfaceGitUrl="https://github.com/pi-hole/AdminLTE.git"
 webInterfaceDir="${webroot}/admin"
-piholeGitUrl="https://github.com/pi-hole/pi-hole.git"
+piholeGitUrl="https://github.com/ilpianista/pi-hole.git"
 PI_HOLE_LOCAL_REPO="/etc/.pihole"
 # These are the names of pi-holes files, stored in an array
 PI_HOLE_FILES=(chronometer list piholeDebug piholeLogFlush setupLCD update version gravity uninstall webpage)
@@ -2227,6 +2227,7 @@ clone_or_update_repos() {
         { printf "  %bUnable to clone %s into %s, unable to continue%b\\n" "${COL_LIGHT_RED}" "${piholeGitUrl}" "${PI_HOLE_LOCAL_REPO}" "${COL_NC}"; \
         exit 1; \
         }
+        fetch_checkout_pull_branch ${PI_HOLE_LOCAL_REPO} alpine
         # If the Web interface was installed,
         if [[ "${INSTALL_WEB_INTERFACE}" == true ]]; then
             # get the Web git files
