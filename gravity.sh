fa57c457f3fc53dcb813c74a8ed804eaae494842
DL6ER <dl6er@dl6er.de>
2020-04-21 09:10:21 +0200

    Boldify number of unique domains as this is the actually interesting number
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- gravity.sh
+++ gravity.sh
@@ -640,7 +640,7 @@ gravity_Table_Count() {
   if [[ "${table}" == "vw_gravity" ]]; then
     local unique
     unique="$(sqlite3 "${gravityDBfile}" "SELECT COUNT(DISTINCT domain) FROM ${table};")"
-    echo -e "  ${INFO} Number of ${str}: ${num} (${unique} unique domains)"
+    echo -e "  ${INFO} Number of ${str}: ${num} (${COL_BOLD}${unique} unique domains${COL_NC})"
     sqlite3 "${gravityDBfile}" "INSERT OR REPLACE INTO info (property,value) VALUES ('gravity_count',${unique});"
   else
     echo -e "  ${INFO} Number of ${str}: ${num}"
