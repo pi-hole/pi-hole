aec5c68f9fcb575a83b971fe0b480b4e2bd9bd3a
Dan Schaper <dan.schaper@pi-hole.net>
2020-05-01 16:28:40 -0700

    Beta v5
    Signed-off-by: Dan Schaper <dan.schaper@pi-hole.net>

--- test/release_check.sh
+++ test/release_check.sh
@@ -1,9 +1,25 @@
-#!/usr/bin/env bash
+#! /usr/bin/env bash
+#apt install lighttpd -y
 
-mkdir /etc/pihole
-echo release/v4.0 | sudo tee /etc/pihole/ftlbranch
-git clone https://github.com/pi-hole/pi-hole.git /etc/.pihole
-cd /etc/.pihole
-git checkout release/v4.0
-bash /etc/.pihole/automated\ install/basic-install.sh
-pihole checkout web release/v4.0 <<< y
+if type apt-get &> /dev/null; then
+  sudo apt update
+  sudo apt -y install git
+elif type yum &> /dev/null; then
+  echo "yum install git"
+  sudo yum install git
+fi
+
+sudo git clone https://github.com/pi-hole/AdminLTE.git /var/www/html/admin
+pushd "$_"
+sudo git checkout release/v5.0
+popd
+
+sudo git clone https://github.com/pi-hole/pi-hole.git /etc/.pihole
+pushd "$_"
+sudo git checkout release/v5.0
+popd
+
+sudo mkdir -pv /etc/pihole
+echo "release/v5.0" |sudo tee /etc/pihole/ftlbranch
+
+/etc/.pihole/automated\ install/basic-install.sh
