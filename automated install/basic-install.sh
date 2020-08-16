ed81e133deae449a5fbd2f73c177106609af0332
Craig Mayhew <craig@mayhew.io>
2020-08-16 16:23:37 +0100

    Typo recieves -> receives
    Signed-off-by: craigmayhew <craig@mayhew.io>

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -299,7 +299,7 @@ if is_command apt-get ; then
             phpVer="php"
         # Else error if we find the php5 package
         elif apt-cache show php5 > /dev/null 2>&1; then
-            printf "  %b Aborting installation: Only PHP5 packages were found in APT repository. PHP5 no longer recieves security patches. Please upgrade to PHP7. \\n" "${CROSS}"
+            printf "  %b Aborting installation: Only PHP5 packages were found in APT repository. PHP5 no longer receives security patches. Please upgrade to PHP7. \\n" "${CROSS}"
             exit 1
         # Else print error and exit
         else
