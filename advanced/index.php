a6b02958e4b9d3e13772e0833c683df14919b179
Sam <48739810+The-SamminAter@users.noreply.github.com>
2020-11-20 13:22:04 -0800

    Fixed the splashpage not appearing properly on non-root directories
    Before, when visiting an invalid non-`/var/www/html/` path page, such as `$serverName/a/a`, the `Did you mean to go to the admin panel?` page didn't show up correctly, as the file paths on lines 66, 67, and 70 didn't start with a `/`. In the example, instead of loading in `$serverName/pihole/blockingpage.css`, it would attempt to load $serverName/a/pihole/blockingpage.css`.
    
    Signed-off-by: The-SamminAter

--- advanced/index.php
+++ advanced/index.php
@@ -63,11 +63,11 @@ if ($serverName === "pi.hole"
             <meta charset='utf-8'>
             $viewPort
             <title>● $serverName</title>
-            <link rel='stylesheet' href='pihole/blockingpage.css'>
-            <link rel='shortcut icon' href='admin/img/favicons/favicon.ico' type='image/x-icon'>
+            <link rel='stylesheet' href='/pihole/blockingpage.css'>
+            <link rel='shortcut icon' href='/admin/img/favicons/favicon.ico' type='image/x-icon'>
         </head>
         <body id='splashpage'>
-            <img src='admin/img/logo.svg' alt='Pi-hole logo' width='256' height='377'>
+            <img src='/admin/img/logo.svg' alt='Pi-hole logo' width='256' height='377'>
             <br>
             <p>Pi-<strong>hole</strong>: Your black hole for Internet advertisements</p>
             <a href='/admin'>Did you mean to go to the admin panel?</a>
