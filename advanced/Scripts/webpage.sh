4093959d3ede4151d5cb4e994f1b2e6ef744b007
Adam Warner <me@adamwarner.co.uk>
2020-11-30 21:55:07 +0000

    Appease stickler
    Signed-off-by: Adam Warner <me@adamwarner.co.uk>

--- advanced/Scripts/webpage.sh
+++ advanced/Scripts/webpage.sh
@@ -239,7 +239,7 @@ trust-anchor=.,20326,8,2,E06D44B80B8F1D39A95C0B0D7C65D08458E880409BBC68345710423
         #          192.in-addr.arpa to 192.0.0.0/8
         search="in-addr.arpa"
         if [[ "$CONDITIONAL_FORWARDING_REVERSE" == *"$search" ]];then
-            arrRev=(${CONDITIONAL_FORWARDING_REVERSE//./ })        
+            arrRev=("${CONDITIONAL_FORWARDING_REVERSE//./ }")        
             case ${#arrRev[@]} in 
                 6   )   REV_SERVER_CIDR="${arrRev[3]}.${arrRev[2]}.${arrRev[1]}.${arrRev[0]}/32";;
                 5   )   REV_SERVER_CIDR="${arrRev[2]}.${arrRev[1]}.${arrRev[0]}.0/24";;
