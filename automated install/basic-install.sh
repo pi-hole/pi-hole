8717c0c9d0d369b6acc07dd1eca805e599e1bbcd
sjt5 <sjt5atra@gmail.com>
2021-01-23 23:05:13 -0500

    Allow installation on RHEL instead of only CentOS and Scientific Linux.

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -403,7 +403,7 @@ elif is_command rpm ; then
         # all required packages should be available by default with the latest fedora release
         : # continue
     # or if host OS is CentOS,
-    elif grep -qiE 'centos|scientific' /etc/redhat-release; then
+    elif grep -qiE 'centos|scientific|rhel' /etc/redhat-release; then
         # Pi-Hole currently supports CentOS 7+ with PHP7+
         SUPPORTED_CENTOS_VERSION=7
         SUPPORTED_CENTOS_PHP_VERSION=7
