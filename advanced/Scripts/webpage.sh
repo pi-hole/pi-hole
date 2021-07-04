a0d74d1e9d52fb0039b6c3f709216e4af2acb0d3
DL6ER <dl6er@dl6er.de>
2021-07-04 13:55:47 +0200

    Canary domain handling is now happening in FTL where we have much finer control over it.
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- advanced/Scripts/webpage.sh
+++ advanced/Scripts/webpage.sh
@@ -273,11 +273,6 @@ trust-anchor=.,20326,8,2,E06D44B80B8F1D39A95C0B0D7C65D08458E880409BBC68345710423
         fi
     fi
 
-    # Prevent Firefox from automatically switching over to DNS-over-HTTPS
-    # This follows https://support.mozilla.org/en-US/kb/configuring-networks-disable-dns-over-https
-    # (sourced 7th September 2019)
-    add_dnsmasq_setting "server=/use-application-dns.net/"
-
     # We need to process DHCP settings here as well to account for possible
     # changes in the non-FQDN forwarding. This cannot be done in 01-pihole.conf
     # as we don't want to delete all local=/.../ lines so it's much safer to
