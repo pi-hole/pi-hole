e6ba68513a29fa9bd8dd43e30550b61296844373
Dan Schaper <dan.schaper@pi-hole.net>
2021-07-26 12:55:14 -0700

    Guard for non-zero return value.
    Signed-off-by: Dan Schaper <dan.schaper@pi-hole.net>

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -2042,8 +2042,10 @@ installPihole() {
     fi
     # Install the cron file
     installCron
-    # Install the logrotate file
-    installLogrotate
+    if ! installLogrotate; then
+        printf "  %b Failure in logrotate installation function.\\n" "${CROSS}"
+        # This isn't fatal, no need to exit.    
+    fi
     # Check if dnsmasq is present. If so, disable it and back up any possible
     # config file
     disable_dnsmasq
