d7d8e9730b385342a79bbac368243f98cade359f
Christian König <ckoenig@posteo.de>
2021-12-30 06:49:03 +0100

    Remove pihole-FTL.conf.5 from automated tests
    Signed-off-by: Christian König <ckoenig@posteo.de>

--- test/test_any_automated_install.py
+++ test/test_any_automated_install.py
@@ -351,10 +351,6 @@ def test_installPihole_fresh_install_readableFiles(host):
             'r', '/usr/local/share/man/man8/pihole-FTL.8', piholeuser)
         actual_rc = host.run(check_man).rc
         assert exit_status_success == actual_rc
-        check_man = test_cmd.format(
-            'r', '/usr/local/share/man/man5/pihole-FTL.conf.5', piholeuser)
-        actual_rc = host.run(check_man).rc
-        assert exit_status_success == actual_rc
     # check not readable sudoers file
     check_sudo = test_cmd.format(
         'r', '/etc/sudoers.d/pihole', piholeuser)
