1c28da7bbd0a3da34787133a68fcf835847facb5
Peter Russell <jpgpi250@gmail.com>
2022-04-20 10:43:39 +0200

    Update gravity.sh
    Co-authored-by: yubiuser <ckoenig@posteo.de>

--- gravity.sh
+++ gravity.sh
@@ -527,7 +527,8 @@ parseList() {
   # This sed does the following things:
   # 1. Remove all domains containing invalid characters. Valid are: a-z, A-Z, 0-9, dot (.), minus (-), underscore (_)
   # 2. Append ,adlistID to every line
-  # 3. Ensures there is a newline on the last line
+  # 3. Remove trailing period (see https://github.com/pi-hole/pi-hole/issues/4701)
+  # 4. Ensures there is a newline on the last line
   # sed -e "/[^a-zA-Z0-9.\_-]/d;s/$/,${adlistID}/;/.$/a\\" "${src}" >> "${target}"
   # previous line modified 20/04/2022, see https://github.com/pi-hole/pi-hole/issues/4701
   sed -e "/[^a-zA-Z0-9.\_-]/d;s/\.$//;s/$/,${adlistID}/;/.$/a\\" "${src}" >> "${target}"
