99c72bf302cb1b4c7fba63c6d452ebf2d76883e4
Christian König <ckoenig@posteo.de>
2022-10-21 12:25:23 +0200

    Escape dots in local DNS records/CNAMES before removing them
    Signed-off-by: Christian König <ckoenig@posteo.de>

--- advanced/Scripts/webpage.sh
+++ advanced/Scripts/webpage.sh
@@ -749,6 +749,12 @@ checkDomain()
     echo "${validDomain}"
 }
 
+escapeDots()
+{
+    escaped=$(sed 's/\./\\./g' <<< "$1" )
+    echo "${escaped}"
+}
+
 addAudit()
 {
     shift # skip "-a"
@@ -824,6 +830,7 @@ RemoveCustomDNSAddress() {
     validHost="$(checkDomain "${host}")"
     if [[ -n "${validHost}" ]]; then
         if valid_ip "${ip}" || valid_ip6 "${ip}" ; then
+            validHost=$(escapeDots "${validHost}")
             sed -i "/^${ip} ${validHost}$/Id" "${dnscustomfile}"
         else
             echo -e "  ${CROSS} Invalid IP has been passed"
@@ -877,7 +884,9 @@ RemoveCustomCNAMERecord() {
     if [[ -n "${validDomain}" ]]; then
         validTarget="$(checkDomain "${target}")"
         if [[ -n "${validTarget}" ]]; then
-            sed -i "/cname=${validDomain},${validTarget}$/Id" "${dnscustomcnamefile}"
+            validDomain=$(escapeDots "${validDomain}")
+            validTarget=$(escapeDots "${validTarget}")
+            sed -i "/^cname=${validDomain},${validTarget}$/Id" "${dnscustomcnamefile}"
         else
             echo "  ${CROSS} Invalid Target Passed!"
             exit 1
