b9ebb0524679e6ad1d318023c9f63ee5de8410f9
Christian König <ckoenig@posteo.de>
2022-12-28 13:42:57 +0100

    Set permission after we know the user wants to install the web server
    Signed-off-by: Christian König <ckoenig@posteo.de>

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -1399,10 +1399,10 @@ installConfigs() {
     install -T -m 0755 "${PI_HOLE_LOCAL_REPO}/advanced/Templates/pihole-FTL-poststop.sh" "${PI_HOLE_INSTALL_DIR}/pihole-FTL-poststop.sh"
 
     # If the user chose to install the dashboard,
+    if [[ "${INSTALL_WEB_SERVER}" == true ]]; then
         # set permissions on /etc/lighttpd/lighttpd.conf so pihole user (other) can read the file
         chmod o+x /etc/lighttpd
         chmod o+r "${lighttpdConfig}"
-    if [[ "${INSTALL_WEB_SERVER}" == true ]]; then
         if grep -q -F "FILE AUTOMATICALLY OVERWRITTEN BY PI-HOLE" "${lighttpdConfig}"; then
             # Attempt to preserve backwards compatibility with older versions
             install -D -m 644 -T ${PI_HOLE_LOCAL_REPO}/advanced/${LIGHTTPD_CFG} "${lighttpdConfig}"
