36956103004a99f59aa2258ff0880745d535f354
DL6ER <dl6er@dl6er.de>
2023-01-24 19:51:01 +0100

    Allow running pihole -g without root (run as user pihole)
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- gravity.sh
+++ gravity.sh
@@ -912,7 +912,7 @@ gravity_ShowCount
 
 # Determine if DNS has been restarted by this instance of gravity
 if [[ -z "${dnsWasOffline:-}" ]]; then
-  "${PIHOLE_COMMAND}" restartdns reload
+  "${PIHOLE_COMMAND}" reloaddns
 fi
 
 gravity_Cleanup
