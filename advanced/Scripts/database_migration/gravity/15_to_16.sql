df92b8ac14988ddeece92b733dec7e8b9fb84d3a
DL6ER <dl6er@dl6er.de>
2023-09-12 20:43:34 +0200

    Add missing creation of view vw_antigravity as well as schema change to vw_adlist
    Signed-off-by: DL6ER <dl6er@dl6er.de>

--- advanced/Scripts/database_migration/gravity/15_to_16.sql
+++ advanced/Scripts/database_migration/gravity/15_to_16.sql
@@ -6,6 +6,20 @@ BEGIN TRANSACTION;
 
 ALTER TABLE adlist ADD COLUMN abp_entries INTEGER NOT NULL DEFAULT 0;
 
+CREATE VIEW vw_antigravity AS SELECT domain, adlist_by_group.group_id AS group_id
+    FROM antigravity
+    LEFT JOIN adlist_by_group ON adlist_by_group.adlist_id = antigravity.adlist_id
+    LEFT JOIN adlist ON adlist.id = antigravity.adlist_id
+    LEFT JOIN "group" ON "group".id = adlist_by_group.group_id
+    WHERE adlist.enabled = 1 AND (adlist_by_group.group_id IS NULL OR "group".enabled = 1) AND adlist.type = 1;
+
+DROP VIEW vw_adlist;
+
+CREATE VIEW vw_adlist AS SELECT DISTINCT address, id, type
+    FROM adlist
+    WHERE enabled = 1
+    ORDER BY id;
+
 UPDATE info SET value = 16 WHERE property = 'version';
 
 COMMIT;
