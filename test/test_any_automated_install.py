159817b7e286c4015030bfe61c6100b7d5b2c028
Christian König <ckoenig@posteo.de>
2023-12-06 20:35:26 +0100

    Use development-v6 as ftl branch for binary test
    Signed-off-by: Christian König <ckoenig@posteo.de>

--- test/test_any_automated_install.py
+++ test/test_any_automated_install.py
@@ -465,7 +465,7 @@ def test_FTL_development_binary_installed_and_responsive_no_errors(host):
     source /opt/pihole/basic-install.sh
     create_pihole_user
     funcOutput=$(get_binary_name)
-    echo "development" > /etc/pihole/ftlbranch
+    echo "development-v6" > /etc/pihole/ftlbranch
     binary="pihole-FTL${funcOutput##*pihole-FTL}"
     theRest="${funcOutput%pihole-FTL*}"
     FTLdetect "${binary}" "${theRest}"
