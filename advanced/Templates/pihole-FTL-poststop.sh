2c32d485bd8d1c5892ae635e3f35065fde004e47
Christian König <github@yubiuser.dev>
2024-07-27 21:55:18 +0200

    Remove obsolet getFTLPIDFile() We can get the path of the PID file via getFTLConfigValue files.pid
    Signed-off-by: Christian König <github@yubiuser.dev>

--- advanced/Templates/pihole-FTL-poststop.sh
+++ advanced/Templates/pihole-FTL-poststop.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
-# Source utils.sh for getFTLPIDFile()
+# Source utils.sh for getFTLConfigValue()
 PI_HOLE_SCRIPT_DIR='/opt/pihole'
 utilsfile="${PI_HOLE_SCRIPT_DIR}/utils.sh"
 # shellcheck disable=SC1090
 . "${utilsfile}"
 
 # Get file paths
-FTL_PID_FILE="$(getFTLPIDFile)"
+FTL_PID_FILE="$(getFTLConfigValue files.pid)"
 
 # Cleanup
 rm -f /run/pihole/FTL.sock /dev/shm/FTL-* "${FTL_PID_FILE}"
