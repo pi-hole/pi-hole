ad6a48b219ea24cb7069eff10e698c16cf80534d
MichaIng <micha@dietpi.com>
2025-02-26 20:59:32 +0100

    Add comment about TLS-related file permissions
    Co-authored-by: Dominik <DL6ER@users.noreply.github.com>
    Signed-off-by: MichaIng <micha@dietpi.com>

--- advanced/Templates/pihole-FTL-prestart.sh
+++ advanced/Templates/pihole-FTL-prestart.sh
@@ -16,6 +16,7 @@ chown -R pihole:pihole /etc/pihole /var/log/pihole
 find /etc/pihole/ /var/log/pihole/ -type d -exec chmod 0755 {} +
 # Set all files (except TLS-related ones) to u+rw g+r
 find /etc/pihole/ /var/log/pihole/ -type f ! \( -name '*.pem' -o -name '*.crt' \) -exec chmod 0640 {} +
+# Set TLS-related files to a more restrictive u+rw *only* (they may contain private keys)
 find /etc/pihole/ /var/log/pihole/ -type f -name '*.pem' -o -name '*.crt' -exec chmod 0600 {} +
 
 # Logrotate config file need to be owned by root
