98b0d498951ff0637c5a13d29e247f6354d073cc
Michael Ziminsky (Z) <mgziminsky@users.noreply.github.com>
2025-07-09 15:28:51 -0700

    Alpine: Ensure community repo is enabled and handle dependency install errors
    Signed-off-by: Michael Ziminsky (Z) <mgziminsky@users.noreply.github.com>

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -1446,8 +1446,19 @@ install_dependent_packages() {
         fi
     # Install Alpine packages
     elif is_command apk; then
-        ${PKG_INSTALL} -q -t "pihole-meta=${PIHOLE_META_VERSION_APK}" "${PIHOLE_META_DEPS_APK[@]}" &> /dev/null &&
+        local pattern='^\s*#(.*/community/?)\s*$'
+        sed -Ei "s:${pattern}:\1:" /etc/apk/repositories
+        if grep -Eq "${pattern}" /etc/apk/repositories; then
+            printf "%b  %b Failed to enable 'community' repo.\\n" "${OVER}" "${CROSS}"
+            printf "  %b %s..." "${INFO}" "${str}"
+        fi
+        if { ${PKG_INSTALL} -q -t "pihole-meta=${PIHOLE_META_VERSION_APK}" "${PIHOLE_META_DEPS_APK[@]}" > /dev/null; }; then
             printf "%b  %b %s\\n" "${OVER}" "${TICK}" "${str}"
+        else
+            printf "%b  %b %s\\n" "${OVER}" "${CROSS}" "${str}"
+            printf "  %b Error: Unable to install Pi-hole dependency package.\\n" "${COL_RED}"
+            return 1
+        fi
     else
         # we cannot install the dependency package
         printf "  %b No supported package manager found\\n" "${CROSS}"
