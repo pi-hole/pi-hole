5d1ef6279f6b9d26b4a106709ddb292a06fbd830
Christian König <github@yubiuser.dev>
2025-08-06 12:57:36 +0200

    Fix shellcheck warnings for pihole bash completion
    Signed-off-by: Christian König <github@yubiuser.dev>

--- automated install/basic-install.sh
+++ automated install/basic-install.sh
@@ -1137,7 +1137,7 @@ installScripts() {
         install -o "${USER}" -Dm755 -t "${PI_HOLE_INSTALL_DIR}" ./automated\ install/uninstall.sh
         install -o "${USER}" -Dm755 -t "${PI_HOLE_INSTALL_DIR}" ./advanced/Scripts/COL_TABLE
         install -o "${USER}" -Dm755 -t "${PI_HOLE_BIN_DIR}" pihole
-        install -Dm644 ./advanced/bash-completion/pihole /etc/bash_completion.d/pihole
+        install -Dm644 ./advanced/bash-completion/pihole.bash /etc/bash_completion.d/pihole
         install -Dm644 ./advanced/bash-completion/pihole-ftl.bash /etc/bash_completion.d/pihole-FTL
         printf "%b  %b %s\\n" "${OVER}" "${TICK}" "${str}"
 
