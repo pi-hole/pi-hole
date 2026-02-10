724a7d5ac1c80faba232f4f73223a386f917b245
Adnan Mazharuddin Shaikh <adnanmazharuddinshaikh@gmail.com>
2026-02-09 22:51:06 -0800

    Fix readonly variable error in network flush (Docker)
    When running 'pihole networkflush' in Docker environments, the command
    fails with:
      /opt/pihole/api.sh: line 25: PI_HOLE_SCRIPT_DIR: readonly variable
    
    This occurs because piholeNetworkFlush.sh declares PI_HOLE_SCRIPT_DIR
    as readonly, then sources api.sh which attempts to assign the same
    variable.
    
    Changed api.sh to use 'readonly' declaration instead of assignment,
    making it consistent with other scripts and preventing the conflict.
    
    Fixes #6523
    
    Signed-off-by: Adnan Mazharuddin Shaikh <adnanmazharuddinshaikh@gmail.com>

--- advanced/Scripts/api.sh
+++ advanced/Scripts/api.sh
@@ -22,7 +22,7 @@ TestAPIAvailability() {
     local chaos_api_list authResponse authStatus authData apiAvailable DNSport
 
     # as we are running locally, we can get the port value from FTL directly
-    PI_HOLE_SCRIPT_DIR="/opt/pihole"
+    readonly PI_HOLE_SCRIPT_DIR="/opt/pihole"
     utilsfile="${PI_HOLE_SCRIPT_DIR}/utils.sh"
     # shellcheck source=./advanced/Scripts/utils.sh
     . "${utilsfile}"
